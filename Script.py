87209071c5a2a856540e3fbe14b52d70891056d3
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-02-16 03:30:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,101 +1,113 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
+    START_TXT = """👋 Hello {},
+🤖 My name is <a href=https://t.me/{}>{}</a>, 𝙸 can provide you movies, Visit my website www.hagadmansa.com. <br> If you don't know how to get movies? just [Click here](https://t.me/hagadmansa/2) to watch a tutorial on YouTube"""
+    HELP_TXT = """👋 Hey {}
+Here is the help for my commands"""
+    ABOUT_TXT = """✯ My Name: {}
+✯ Creator: <a href=https://t.me/himanshurastogiofficial>Himanshu Rastogi</a>
+✯ Library: <a href=https://pyrogram.org>Pyrogram</a>
+✯ Language: <a href=https:/python.org>Python 3</a>
+✯ Database: <a href=https://mongodb.com/>MongoDB</a>
+✯ Server: <a href=https://heroku.com/>Heroku</a>
+✯ Build Status: v1.0.1 [Beta]"""
+    SOURCE_TXT = """<b>❗️ NOTE:</b>
+    
 - Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- Source - https://github.com/HagadMansa/EvaMaria  
 
-<b>DEVS:</b>
+<b>👨‍💻 DEVS:</b>
+
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+    MANUELFILTER_TXT = """<b>ℹ️ Help</b>:Filters
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+🔑 Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
-<b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+<b❗️ NOTE:</b>
 
-<b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
+1. Your Bot should have admin privillage.
+2. Only admins can add filters in a chat.
+3. Alert buttons have a limit of 64 characters.
 
-- Eva Maria Supports both url and alert inline buttons.
+<b>🧩 Commands and Usage:</b>
 
-<b>NOTE:</b>
+• /filter - add a filter in chat
+• /filters - list all the filters of a chat
+• /del - delete a specific filter in chat
+• /delall - delete the whole filters in a chat (chat owner only)"""
+    BUTTON_TXT = """<b>ℹ️ Help</b>: Buttons
+
+- This Bot Supports both url and alert inline buttons.
+
+<b>❗️ NOTE:</b>
+
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+2. This Bot supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format.
 
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<b>🔗 URL buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/hagadmansa)</code>
 
-<b>Alert buttons:</b>
+<b>🔔 Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+    AUTOFILTER_TXT = """<b>ℹ️ Help</b>: Auto Filter
 
-<b>NOTE:</b>
+<b>❗️ NOTE:</b>
+
 1. Make me the admin of your channel if it's private.
 2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
+    CONNECTION_TXT = """<b>ℹ️ Help</b>: Connections
 
 - Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+- It helps to avoid spamming in groups.
 
-<b>NOTE:</b>
+<b>❗️ NOTE:</b>
+
 1. Only admins can add a connection.
 2. Send <code>/connect</code> for connecting me to ur PM
 
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+<b>🧩 Commands and Usage:</b>
 
-<b>NOTE:</b>
-these are the extra features of Eva Maria
+• /connect  - connect a particular chat to your PM.
+• /disconnect  - disconnect from a chat.
+• /connections - list all your connections."""
+    EXTRAMOD_TXT = """<b>ℹ️ Help</b>: >Extra Modules
 
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+<b>❗️ NOTE:</b>
 
-<b>NOTE:</b>
-This module only works for my admins
+- These are the extra features of This Bot.
 
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+<b>🧩 Commands and Usage:</b>
+
+• /id - get id of a specified user.
+• /info  - get information about a user.
+• /imdb  - get the film information from IMDb source.
+• /search  - get the film information from various sources."""
+    ADMIN_TXT = """<b>ℹ️ Help</b>: Admin
+
+<b>❗️ NOTE:</b>
+
+- This module only works for my admins
+
+<b>🧩 Commands and Usage:</b>
+
+• /logs - to get log file, you can check recent errors.
+• /stats - to get status of all files in all databases.
+• /delete - to delete a specific file from database.
+• /users - to get list of all users and ids.
+• /chats - to get list of all chats and ids.
+• /leave  - to leave from a chat.
+• /disable  -  do disable a chat.
+• /ban  - to ban a user.
+• /unban  - to unban a user.
+• /channel - to get list of total connected channels.
+• /broadcast - to broadcast a message to all users."""
+    STATUS_TXT = """★ Total Files: {}
+★ Total Users: {}
+★ Total Chats: {}
+★ Used Storage: {} MiB
+★ Free Storage: {} MiB"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
