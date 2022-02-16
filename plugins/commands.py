c6b9d03836c2687b46bb5772d67979b135af743f
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-02-16 15:10:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ async def start(client, message):
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await asyncio.sleep(2) # 😢 https://github.com/Hagadmansa/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🔅 Share', url='https://t.me/share/url?url=t.me/hagadmansa&text=👋Hey, check out this amazing bot to download free movies.}'),
+            InlineKeyboardButton('🔅 Share', url='https://t.me/share/url?url=t.me/hagadmansa&text=👋Hey, check out this amazing bot to download free movies.'),
             InlineKeyboardButton('📣 Updates', url='https://t.me/hagadmansa')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
@@ -83,7 +83,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('🔅 Share', url='https://t.me/share/url?url=t.me/hagadmansa&text=👋Hey, check out this amazing bot to download free movies.}'),
+            InlineKeyboardButton('🔅 Share', url='https://t.me/share/url?url=t.me/hagadmansa&text=👋Hey, check out this amazing bot to download free movies.'),
             InlineKeyboardButton('📣 Updates', url='https://t.me/hagadmansa')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
