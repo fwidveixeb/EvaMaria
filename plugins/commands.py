119c46aee6b637ef5b42d30f6799b7b5b9f41d9a
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-02-23 02:42:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🔅 Share', url='https://hagadmansa.com'),
+            InlineKeyboardButton('🌐 Website', url='https://hagadmansa.com'),
             InlineKeyboardButton('📣 Updates', url='https://t.me/hagadmansa')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
@@ -83,7 +83,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('🔅 Share', url='https://hagadmansa.com'),
+            InlineKeyboardButton('🌐 Website', url='https://hagadmansa.com'),
             InlineKeyboardButton('📣 Updates', url='https://t.me/hagadmansa')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
