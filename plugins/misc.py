f8398fc1dc2f069e42cdf58f2ca28315a4f0e5d7
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-19 02:13:17 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -156,7 +156,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             [
                 InlineKeyboardButton(
                     text=f"📥 Download Now",
-                    url="https://hagadmansa.com/?s={title}",
+                    url="https://hagadmansa.com/?s={imdb.get('title')}",
                 )
             ]
         ]
