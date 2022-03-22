4ac94d93c0e941a67b68408f54ddd77aaa3964fc
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-22 07:39:12 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -155,7 +155,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{imdb.get('title'.replace(' ', '-'))}",
+                    text=f"{imdb.get('title')}",
                     url=imdb['url'],
                 )
             ] 
@@ -163,7 +163,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     message = quer_y.message.reply_to_message or quer_y.message
     if imdb:
         caption = IMDB_TEMPLATE.format(
-            query = imdb['title'.replace(' ', '-')],
+            query = imdb['title'.replace(' ', '+')],
             title = imdb['title'],
             votes = imdb['votes'],
             aka = imdb["aka"],
