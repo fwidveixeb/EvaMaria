dfec0da57731db24c3cd9fa50e71e7545d9637ad
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-26 12:36:35 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -64,7 +64,7 @@ def get_media_from_message(message: "Message") -> Any:
     group=4,
 )
 async def media_receive_handler(_, m: Message):
-    log_msg = await m.copy_message(Var.BIN_CHANNEL)
+    log_msg = await m.copy_message(chat_id=Var.BIN_CHANNEL)
     stream_link = f"https://download.hagadmansa.com/{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"https://download.hagadmansa.com/{get_hash(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
