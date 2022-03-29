e27486858d56808cac2604d0d666a2168c3106c9
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-29 10:42:21 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -70,7 +70,7 @@ async def media_receive_handler(b, m: Message):
     stream_link = f"https://download.hagadmansa.com/{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"https://download.hagadmansa.com/{get_hash(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
-    string += file_id
+    string = file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
   
     await log_msg.reply_text(
