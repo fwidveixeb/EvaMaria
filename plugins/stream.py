1704f4a4a0a14a575c308de347ad4a98da6f9ca9
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-02 00:05:29 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -3,6 +3,7 @@ import base64
 import urllib.parse
 from pyrogram import Client
 import logging
+from config import Config
 from typing import Any, Optional
 from pyrogram import filters
 from Vars import Var
