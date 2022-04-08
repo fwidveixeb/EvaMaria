73558b6014eefea8304d949de8ef9c125c3bfa0e
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-09 01:48:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -141,7 +141,7 @@ async def advantage_spoll_choker(bot, query):
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
-    if data == "close_data":
+    if query.data == "close_data":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
