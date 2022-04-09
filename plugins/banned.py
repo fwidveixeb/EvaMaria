91a7d4dee2a597271c1c8863919a647eb3df345c
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-09 13:01:27 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,9 +1,10 @@
 from pyrogram import Client, filters
+import random
 from utils import temp
 from pyrogram.types import Message
 from database.users_chats_db import db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from info import SUPPORT_CHAT
+from info import SUPPORT_CHAT, PICS
 async def banned_users(_, client, message: Message):
     return (
         message.from_user is not None or not message.sender_chat
@@ -37,3 +38,77 @@ async def grp_bd(bot, message):
     except:
         pass
     await bot.leave_chat(message.chat.id)
+
+NEW_ABOUT_TEXT = """Hello This command is under testing."""
+
+NEW_ABOUT_HOME = """Hello This command is under testing."""
+
+RATING_TEXT = """This is rating text."""
+
+SOURCE_TEXT = """This is source text."""
+
+DONATE_TEXT = """This is donate text."""
+
+NEW_ABOUT_HOME_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('⭐️ Rating', callback_data='rating'),
+            InlineKeyboardButton('❤️ Source', callback_data='source'),
+            ],[
+            InlineKeyboardButton('💰 Donate', callback_data='donate')
+            ]]
+       )     
+RATING_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
+            ]]
+        )
+SOURCE_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
+            ]]
+        )
+DONATE_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
+            ]]
+        )
+
+@Client.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "new_about_home":
+        await update.message.edit_text(
+            text=NEW_ABOUT_HOME,
+            disable_web_page_preview=True,
+            reply_markup=NEW_ABOUT_HOME_BUTTONS
+        )
+    elif update.data == "rating":
+        await update.message.edit_text(
+            text=RATING_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=RATING_BUTTONS
+        )
+    elif update.data == "source":
+        await update.message.edit_text(
+            text=SOURCE_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=SOURCE_BUTTONS
+        )
+    elif update.data == "donate":
+        await update.message.edit_text(
+            text=DONATE_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=DONATE_BUTTONS
+        )
+
+@Client.on_message(filters.command("about"))
+async def start(client, message):
+        await message.reply_photo(
+        photo=random.choice(PICS),
+        caption=(NEW_ABOUT_TEXT),
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('⭐️ Rating', callback_data='rating'),
+            InlineKeyboardButton('❤️ Source', callback_data='source'),
+            ],[
+            InlineKeyboardButton('💰 Donate', callback_data='donate')
+        ]]))
