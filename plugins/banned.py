6f1413f05883dd79d07564dc1b4b0568c50f2a10
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-09 13:08:24 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -76,24 +76,28 @@ DONATE_BUTTONS = InlineKeyboardMarkup(
 @Client.on_callback_query()
 async def cb_data(bot, update):
     if update.data == "new_about_home":
+        await query.answer('www.hagadmansa.com')
         await update.message.edit_text(
             text=NEW_ABOUT_HOME,
             disable_web_page_preview=True,
             reply_markup=NEW_ABOUT_HOME_BUTTONS
         )
     elif update.data == "rating":
+        await query.answer('www.hagadmansa.com')
         await update.message.edit_text(
             text=RATING_TEXT,
             disable_web_page_preview=True,
             reply_markup=RATING_BUTTONS
         )
     elif update.data == "source":
+        await query.answer('www.hagadmansa.com')
         await update.message.edit_text(
             text=SOURCE_TEXT,
             disable_web_page_preview=True,
             reply_markup=SOURCE_BUTTONS
         )
     elif update.data == "donate":
+        await query.answer('www.hagadmansa.com')
         await update.message.edit_text(
             text=DONATE_TEXT,
             disable_web_page_preview=True,
@@ -102,6 +106,7 @@ async def cb_data(bot, update):
 
 @Client.on_message(filters.command("about"))
 async def start(client, message):
+    await query.answer('www.hagadmansa.com')
         await message.reply_photo(
         photo=random.choice(PICS),
         caption=(NEW_ABOUT_TEXT),
