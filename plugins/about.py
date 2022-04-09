38441974678d21c494b1b823a0cdf9e79d3ea322
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-09 16:49:27 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -13,6 +13,20 @@ SOURCE_TEXT = """This is source text."""
 
 DONATE_TEXT = """This is donate text."""
 
+NEW_HELP_TEXT = """<b>🧩 Here is the help of my commands. Send /about to know about me.</b>"""
+
+NEW_HELP_HOME = """<b>🧩 Here is the help of my commands. Send /about to know about me.</b>"""
+
+FILE_STREAM_TEXT = """This is file stream text."""
+
+FILE_STORE_TEXT = """This is file store text."""
+
+INSTRUCTONS_TEXT = """This is instructions text."""
+
+TUTORIALS_TEXT = """This is tutorials text."""
+
+WARNING_TEXT = """This is warning text."""
+
 NEW_ABOUT_HOME_BUTTONS = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton('⭐️ Rating', callback_data='rating'),
@@ -34,6 +48,36 @@ SOURCE_BUTTONS = InlineKeyboardMarkup(
 DONATE_BUTTONS = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
+            ]]
+        )
+
+FILE_STREAM_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
+            ]]
+        )
+
+FILE_STORE_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
+            ]]
+        )
+
+INSTRUCTIONS_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
+            ]]
+        )
+
+TUTORIALS_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
+            ]]
+        )
+
+WARNING_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
             ]]
         )
 
@@ -67,6 +111,49 @@ async def cb_data(bot, update):
             disable_web_page_preview=True,
             reply_markup=DONATE_BUTTONS
         )
+     elif update.data == "new_help_home":
+        await update.message.edit_text(
+            text=NEW_HELP_HOME,
+            disable_web_page_preview=True,
+            reply_markup=NEW_HELP_HOME_BUTTONS
+        )
+    elif update.data == "file_stream":
+        await update.message.edit_text(
+            text=FILE_STREAM_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=FILE_STREAM_BUTTONS
+        )
+    elif update.data == "file_store":
+        await update.message.edit_text(
+            text=FILE_STORE_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=FILE_STORE_BUTTONS
+        )
+    elif update.data == "instructions":
+        await update.message.edit_text(
+            text=INSTRUCTONS_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=INSTRUCTIONS_BUTTONS
+        )
+    elif update.data == "tutorials":
+        await update.message.edit_text(
+            text=TUTORIALS_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=TUTORIALS_BUTTONS
+        )
+    elif update.data == "warning":
+        await update.message.edit_text(
+            text=WARNING_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=WARNING_BUTTONS
+        )
+    elif update.data == "close":
+        await update.answer('www.hagadmansa.com')
+        await update.message.delete()
+        try:
+            await update.message.reply_to_message.delete()
+        except:
+            pass
 
 @Client.on_message(filters.command("about"))
 async def start(client, message):
@@ -80,3 +167,19 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('💰 Donate', callback_data='donate')
         ]]))
+
+@Client.on_message(filters.command("help"))
+async def start(client, message):
+        await message.reply_photo(
+        photo=random.choice(PICS),
+        caption=(NEW_HELP_TEXT),
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('📥 File Stream', callback_data='file_stream'),
+            InlineKeyboardButton('📦 File Store', callback_data='file_store'),
+            ],[
+            InlineKeyboardButton('⚙️ Instructions', callback_data='instructions'),
+            InlineKeyboardButton('🕹 Tutorials', callback_data='tutorials'),
+            ],[
+            InlineKeyboardButton('⚠️ Warning', callback_data='warning')
+         ]]))
