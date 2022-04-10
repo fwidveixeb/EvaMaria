86a88fe3d5f796bbdedc9a045e15ff3cecefefb8
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-10 16:57:13 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -21,16 +21,8 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply_text(
-            text=f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}',
-            reply_markup=InlineKeyboardMarkup(
-                         [[
-                    InlineKeyboardButton('💬 Support', url=f'https://t.me/hagadmansachat'),
-                    InlineKeyboardButton('🔐 Close', callbaack_data='close')
-                  ]],
-            quote=True,
-            disable_web_page_preview=True
-                       )
+    await message.reply_text(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
+    
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
