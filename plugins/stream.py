43fb207ba1229a22b3f757d87f3b0439cce9701f
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-11 23:33:04 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -59,10 +59,57 @@ def get_file_id(message):
 async def banned_users(_, client, message: Message):
     return (
         message.from_user is not None or not message.sender_chat
-    ) and message.from_user
+    ) and message.from_user.id in temp.BANNED_USERS
 
 banned_user = filters.create(banned_users)
 
+YES_PHOTO = ["https://telegra.ph/file/2e8725f268df2e9e693f1.jpg"]
+
+YES_TEXT = f"{} \n\nFile has beendeleted successfully."
+
+DELETE_TEXT = f"{} \n\nDo you really want to delete this file?"
+
+DELETE_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('✅ Yes', callback_data='yes'),
+            InlineKeyboardButton('❌ No', callback_data='no'),
+        ]]
+      )
+
+NO_TEXT = f"{}"
+
+NO_BUTTON = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🗑 Delete File', callback_data='delete')
+                    ]
+                ]
+            )
+
+@Client.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "yes":
+        await update.answer('File Deleted Successfully')
+        media=random.choice(YES_PHOTO)
+        newtext=f"User: **{update.from_user.mention(style='md')}** Track: **#u{update.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+        await update.edit_message_media(
+        media=InputMediaPhoto(media=media, caption=YES_TEXT).format(newtext),
+        )
+    elif update.data == "no":
+        await update.answer('Cancel file deleting process.')
+        newtext=f"User: **{update.from_user.mention(style='md')}** Track: **#u{update.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+        await update.edit_text(
+        text=NO_TEXT.format(newtext),
+        reply_markup=NO_BUTTONS,
+        )
+    elif update.data == "delete":
+        await update.answer('Do you really want to delete this file?')
+        newtext=f"User: **{update.from_user.mention(style='md')}** Track: **#u{update.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+        await update.edit_text(
+        text=DELETE_TEXT.format(newtext),
+        reply_markup=DELETE_BUTTONS
+        )
+        
 @Client.on_message( filters.private & ( filters.document | filters.video | filters.audio ) & ~banned_user, group=4,)
 async def media_receive_handler(b, m):
     
@@ -72,6 +119,19 @@ async def media_receive_handler(b, m):
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     newtext=f"User: **{m.from_user.mention(style='md')}** Track: **#u{m.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+    await log_msg.reply_text(
+            text=f"User: **{m.from_user.mention(style='md')}** Track: **#u{m.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**",
+            quote=True,
+            parse_mode="markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🗑 Delete File', callback_data='delete')
+                    ]
+                ]
+            )
+    )
     
     await m.reply_text(
         text="""<b>🤓 I generated link for you, just reply the file with /link to generate an extra link.</b>""",
@@ -87,12 +147,13 @@ async def media_receive_handler(b, m):
         )
     await log_msg.edit_text(
         text=f"{newtext}",
-        reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton('🗑 Delete File', callback_data='delete')
-                    ]
-                ]
-            )
+        reply_markup=DELETE
         )
-    
+EMOJI = ["🎲", "🎯", "🏀", "⚽", "🎳", "🎰", "🎲"]
+
+@Client.on_message(filters.command("emoji"))
+async def emoji(bot, message):
+    await reply_text(
+    chat_id=message.chat_id,
+    text=random.choice(EMOJI)
+    )
