b5b81bd9b20e88c066cf80d42444de037c1706a0
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-11 17:19:45 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -118,7 +118,7 @@ async def test(client, bot):
 
 @Client.on_message(filters.channel & (filters.document | filters.video) & ~filters.edited, group=-1)
 async def channel_receive_handler(bot, broadcast):
-    await bot.edit_message_reply_markup(
+    await bot.edit_text(
         text=f"User: **{broadcast.from_user.mention(style='md')}** Track: **#u{broadcast.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**",
         chat_id=broadcast.chat.id,
         message_id=broadcast.message_id,
