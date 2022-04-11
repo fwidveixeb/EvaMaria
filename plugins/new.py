f04549cf54ff55ac532b7ff25fd078665aab1365
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-11 22:02:05 +0530

    Update new.py

--- plugins/new.py
+++ plugins/new.py
@@ -1,8 +1,95 @@
 import random
 import asyncio
+from Vars import Var
 from info import PICS, ADMINS
 from pyrogram import filters, Client
+from utils import temp
+from pyrogram.file_id import FileId
+from urllib.parse import quote_plus
+from database.ia_filterdb import unpack_new_file_id
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+
+def get_hash(media_msg: Message) -> str:
+    media = get_media_from_message(media_msg)
+    return getattr(media, "file_unique_id", "")[:6]
+
+def get_name(media_msg: Message) -> str:
+    media = get_media_from_message(media_msg)
+    return getattr(media, "file_name", "")
+
+class FIleNotFound(Exception):
+    message = "File not found"
+
+async def get_file_ids(client: Client, chat_id: int, message_id: int) -> Optional[FileId]:
+    message = await client.get_messages(chat_id, message_id)
+    if message.empty:
+        raise FIleNotFound
+    media = get_media_from_message(message)
+    file_unique_id = await parse_file_unique_id(message)
+    file_id = await parse_file_id(message)
+    setattr(file_id, "file_size", getattr(media, "file_size", 0))
+    setattr(file_id, "mime_type", getattr(media, "mime_type", ""))
+    setattr(file_id, "file_name", getattr(media, "file_name", ""))
+    setattr(file_id, "unique_id", file_unique_id)
+    return file_id
+
+def get_media_from_message(message: "Message") -> Any:
+    media_types = (
+        "audio",
+        "document",
+        "photo",
+        "sticker",
+        "animation",
+        "video",
+        "voice",
+        "video_note",
+    )
+    for attr in media_types:
+        media = getattr(message, attr, None)
+        if media:
+            return media
+        
+def get_file_id(message):
+    media=message.document or message.audio or message.video
+    return media.file_id
+
+async def banned_users(_, client, message: Message):
+    return (
+        message.from_user is not None or not message.sender_chat
+    ) and message.from_user.id in temp.BANNED_USERS
+
+banned_user = filters.create(banned_users)
+
+YES_PHOTO = ["https://telegra.ph/file/2e8725f268df2e9e693f1.jpg"]
+
+YES_TEXT = """{} \n\nFile has beendeleted successfully."""
+
+DELETE_TEXT = """{} \n\nDo you really want to delete this file?"""
+
+DELETE_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('✅ Yes', callback_data='yes'),
+            InlineKeyboardButton('❌ No', callback_data='no'),
+        ]]
+      )
+
+NO_TEXT = """{}"""
+
+DELETE = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🗑 Delete File', callback_data='delete')
+                    ]
+                ]
+            )
+
+NO_BUTTON = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🗑 Delete File', callback_data='delete')
+                    ]
+                ]
+            )
 
 NEW_ABOUT_TEXT = """<b>😊 Use these buttons to know about me. Send /start to reload me.</b>"""
 
@@ -151,6 +238,30 @@ async def cb_data(bot, update):
         await update.edit_message_media(
         media=InputMediaPhoto(media=image, caption=WARNING_TEXT),
         reply_markup=HELP_BACK_BUTTONS
+        )
+    elif update.data == "yes":
+        await update.answer('File Deleted Successfully')
+        media=random.choice(YES_PHOTO)
+        log_msg = await bot.copy_message(chat_id=Var.BIN_CHANNEL, from_chat_id=update.chat.id, message_id=update.message_id)
+        newtext=f"User: **{update.from_user.mention(style='md')}** Track: **#u{update.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+        await update.edit_message_media(
+        media=InputMediaPhoto(media=media, caption=YES_TEXT).format(newtext),
+        )
+    elif update.data == "no":
+        await update.answer('Cancel file deleting process.')
+        log_msg = await bot.copy_message(chat_id=Var.BIN_CHANNEL, from_chat_id=update.chat.id, message_id=update.message_id)
+        newtext=f"User: **{update.from_user.mention(style='md')}** Track: **#u{update.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+        await update.edit_text(
+        text=NO_TEXT.format(newtext),
+        reply_markup=NO_BUTTONS,
+        )
+    elif update.data == "delete":
+        await update.answer('Do you really want to delete this file?')
+        log_msg = await bot.copy_message(chat_id=Var.BIN_CHANNEL, from_chat_id=update.chat.id, message_id=update.message_id)
+        newtext=f"User: **{update.from_user.mention(style='md')}** Track: **#u{update.chat.id}** Hash: **#{get_hash(log_msg)}{log_msg.message_id}** Link: **[Hold Me]({short_link})**"
+        await update.edit_text(
+        text=DELETE_TEXT.format(newtext),
+        reply_markup=DELETE_BUTTONS
         )
     elif update.data == "close":
         await update.answer('www.hagadmansa.com')
