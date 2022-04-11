2e1c43196b2de57b2d299af1606e02e27e9bcc8f
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-11 17:30:29 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -111,7 +111,6 @@ async def media_receive_handler(b, m: Message):
     await m.edit_message_reply_markup(
         text="""Hello this message has forwarded from """,
         chat_id=b.chat.id,
-        message_id=b.message_id,
         reply_markup=DELETE
         )
     
