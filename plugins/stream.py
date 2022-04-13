decdb75738fe9d8878208bb0269e859de94ee673
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-14 01:21:05 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -92,7 +92,7 @@ async def cb_data(bot, update):
 async def media_receive_handler(b, m):
     
     banned_user = filters.create(banned_users)
-    log_msg = await b.copy_message(chat_id=ADMINS, from_chat_id=m.chat.id, message_id=m.message_id)
+    log_msg = await b.copy_message(chat_id=Var.BIN_CHANNEL, from_chat_id=m.chat.id, message_id=m.message_id)
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
