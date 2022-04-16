faad181d54b99a5373a6a4fed6d413bb6143a1dd
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-16 23:26:15 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.document & filters.private)
 def document(bot, message):
-  await message.reply(message.document.file_id)
+  message.reply(message.document.file_id)
