134ee36377c028641ba47848134ad1422e8d0545
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-16 23:38:03 +0530

    Update testing.py

--- plugins/testing.py
+++ plugins/testing.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
 
-@Client.on_message(filters.document & filters.private)
-async def document(bot, message):
-  await message.reply(message.document.file_id)
+@Client.on_message(filters.command('docs'))
+def document(bot, message):
+  bot.send_document(message.chat.id, "BQACAgQAAx0CXHCEYQACBPBiIvV5YpuV9LuD7wUKr42XMz1D1gACyAkAAnftCVMZyZfmh1asvR4E")
