61147c8cef3aed14276f245271eda9250021a60d
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-22 23:03:19 +0530

    Update Vars.py

--- Vars.py
+++ Vars.py
@@ -8,4 +8,5 @@ class Var(object):
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", None))
     URL = f"https://download.hagadmansa.com/"
     BANNED_ID = os.environ.get("BANNED_ID", "")
+    TARGET_CHANNEL = int(environ.get("TARGET_CHANNEL", None))
      
