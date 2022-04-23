e7eb5e30fb3e1e5d08c5d255cc3fced71784bd02
bakrapalan <104255719+bakrapalan@users.noreply.github.com>
2022-04-23 21:18:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/bakrapalan/EvaMaria.git /EvaMaria
+  git clone https://github.com/bakrapalan/AutoForwardBot.git /AutoForwardBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /AutoForwardBot
 fi
-cd /EvaMaria
+cd /AutoForwardBot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot This May Take Time Depending On Database Size..."
 python3 bot.py
