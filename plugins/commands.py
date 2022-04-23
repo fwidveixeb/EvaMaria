41382ee94c0be855a5a7582972f07416d9f4b63b
bakrapalan <104255719+bakrapalan@users.noreply.github.com>
2022-04-23 21:12:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,14 +7,14 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from info import ADMINS, LOG_CHANNEL, BATCH_FILE_CAPTION, TARGET_CHANNEL, get_size
+from info import ADMINS, LOG_CHANNEL, FILE_CAPTION, TARGET_CHANNEL, get_size
 from database.connections_mdb import active_connection
 import re
 import json
 import base64
 logger = logging.getLogger(__name__)
 
-BATCH_FILES = {}
+INDEX_FILES = {}
 
 IMAGE = ["https://telegra.ph/file/cd0ef3ccc5727f037cc96.jpg"]
 
@@ -22,7 +22,6 @@ IMAGE = ["https://telegra.ph/file/cd0ef3ccc5727f037cc96.jpg"]
 async def start(bot, message):
     if message.chat.type in ['group', 'supergroup']:
         await message.reply("""I don't work in Gorups or Channels.""")
-        await asyncio.sleep(2)
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, "#NewChat")       
@@ -80,7 +79,7 @@ async def start(bot, message):
     if data.split("-", 1)[0] == "BATCH":
         sts = await message.reply("🙂 I am sending files in your TARGET CHANNEL, when it will complete i will notify you via a message. If i am not sending files in your TARGET CHANNEL then check your logs.")
         file_id = data.split("-", 1)[1]
-        msgs = BATCH_FILES.get(file_id)
+        msgs = INDEX_FILES.get(file_id)
         if not msgs:
             file = await bot.download_media(file_id)
             try: 
@@ -90,14 +89,14 @@ async def start(bot, message):
                 await sts.edit("FAILED")
                 return await bot.send_message(LOG_CHANNEL, "UNABLE TO OPEN FILE.")
             os.remove(file)
-            BATCH_FILES[file_id] = msgs
+            INDEX_FILES[file_id] = msgs
         for msg in msgs:
             title = msg.get("title")
             size=get_size(int(msg.get("size", 0)))
             f_caption=msg.get("caption", "")
             if BATCH_FILE_CAPTION:
                 try:
-                    f_caption=BATCH_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+                    f_caption=FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
                 except Exception as e:
                     logger.exception(e)
                     f_caption=f_caption
@@ -113,7 +112,7 @@ async def start(bot, message):
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
-                await client.send_cached_media(
+                await bot.send_cached_media(
                     chat_id=TARGET_CHANNEL,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
