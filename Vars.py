e8a6680bb9587689348f17411bdd4ce09fee4803
bakrapalan <104255719+bakrapalan@users.noreply.github.com>
2022-04-23 16:51:28 +0530

    Update Vars.py

--- Vars.py
+++ Vars.py
@@ -9,3 +9,13 @@ class Var(object):
     BANNED_ID = os.environ.get("BANNED_ID", "")
     TARGET_CHANNEL = int(environ.get("TARGET_CHANNEL", None))
      
+def get_size(size):
+    """Get size in readable format"""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
