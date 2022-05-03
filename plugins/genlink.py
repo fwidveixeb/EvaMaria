67697e7bab2fef562ee9558f1c1799ad285a1be9
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-03 21:32:31 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -42,7 +42,6 @@ async def gen_link_s(bot, message):
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
     file_link = f'https://t.me/{temp.U_NAME}?start={outstr}'
-    await bot.copy_message(chat_id=Var.BIN_CHANNEL, from_chat_id=message.chat.id, message_id=message.reply_to_message_id )
     await message.reply(
              text=f"<code>{file_link}</code>",
              quote=True,
