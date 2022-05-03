9ef9c4a3196c36e3234224f9ae91a68a4983eb7b
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-03 23:32:54 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -5,8 +5,7 @@ from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from info import NULL
-from datetime
-import datetime
+from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
 logger = logging.getLogger(__name__)
