d224fd333c1a16028d16d9f58c56dd21aa8f6fe5
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-04 13:39:41 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -131,7 +131,8 @@ async def who_is(client, message):
 @Client.on_message(filters.command(["imdb"]))
 async def imdb_search(client, message):
     await message.delete()
-    except message.reply(e)
+    except Exception as e:
+        await message.reply(f"Error - {e}")
     if ' ' in message.text:
         k = await message.reply('Searching ImDB')
         r, title = message.text.split(None, 1)
