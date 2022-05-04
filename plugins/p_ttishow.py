e5fc85648ed7ca3be7ec7e27164a8a66bce8f176
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-04 14:02:05 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1095,7 +1095,8 @@ async def ban_a_user(bot, message):
             chat_id=chat,
             text=default
         )
+        await bot.reply(f'Message sent to {chat} successfully')
     except Exception as e:
         await message.reply(f'Error - {e}')
-        await bot.reply(f'Message sent to {chat} successfully')
+        
         
