f94af3f55216c17157d4489fa029a94560667440
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-06 00:39:42 +0530

    Update extra.py

--- plugins/extra.py
+++ plugins/extra.py
@@ -1,7 +1,1 @@
 from pyrogram import Client, filters
-from pyrogram.handlers import MessageHandler
-
-@Client.on_message(filters.text & filters.private, group=1)
-async def echo_reversed(client, message):
-    await message.reply(message.text[::1])
-    
