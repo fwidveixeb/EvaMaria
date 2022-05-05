655950c38d4229ccb1c13f9ace729d6e58cccf18
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-06 00:35:56 +0530

    Update extra.py

--- plugins/extra.py
+++ plugins/extra.py
@@ -5,10 +5,3 @@ from pyrogram.handlers import MessageHandler
 async def echo_reversed(client, message):
     await message.reply(message.text[::1])
     
-@Client.on_edited_message(filters.text & filters.photo)
-async def text_and_photo(bot, message):
-  await message.reply('Why you edited the message?')
-
-@Client.on_deleted_messages(filters.text & filters.photo)
-async def ndndjencue(bot, message):
-  await message.reply('why you deleted message?')
