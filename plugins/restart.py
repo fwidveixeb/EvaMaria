826fb786e721a31f6f1b72fccf9cf03a9b399cb3
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-08 23:09:01 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,8 +1,9 @@
 from pyrogram import Client, filters
+from Bot import app
 
 @Client.on_message(filters.command("restart"))
 async def restart(bot, message):
   a = await message.reply("Restarting Bot, this may take some time...")
-  await bot.restart()
+  await app.restart()
   await a.delete()
   await message.reply("Restarted successfully.")
