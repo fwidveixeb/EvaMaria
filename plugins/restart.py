c0852f75a19ef538eab7bc30b3e163dc754b8f97
JahilBihari <104792374+JahilBihari@users.noreply.github.com>
2022-05-08 23:10:42 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 
 ...
 
-@app.on_message(filters.command("restart"))
-async def restart_handler(_, m):
-    await m.reply_text("Restarted!", True)
+@Client.on_message(filters.command("restart"))
+async def restart_handler(bot, message):
+    await message.reply_text("Restarted!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
