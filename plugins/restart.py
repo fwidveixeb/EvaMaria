fa72fbadcfced13d8130d8d15694f9d9911db25e
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-17 22:10:30 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,10 +1,10 @@
 import os
-import sys
 from pyrogram import filters, Client
 
 ...
 
 @Client.on_message(filters.command("restart"))
 async def restart(bot, message):
-    await message.reply_text("Restarted!", True)
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    k = await message.reply_text("Restarting...!")
+    os.system("python3 -m app")
+    await k.edit_text("Restarted Successfully")
