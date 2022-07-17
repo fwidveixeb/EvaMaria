f736bfb4b039fc2619b0178d83032a12f9900296
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-17 23:17:31 +0530

    Create ping.py

---
+++ plugins/Devs/ping.py
@@ -0,0 +1,10 @@
+import time
+from info import ADMINS
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("ping") & filters.users(ADMINS))
+async def ping(_, message):
+    start_time = time.time()
+    end_time = time.time()
+    time_taken = (end_t - start_t) * 1000
+    await message.reply_text(f"Pong!\n{time_taken:.3f} ms")
