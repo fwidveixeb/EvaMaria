69f88adc193dfb2f9b74774968eeaf1c0803ce64
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-17 23:20:33 +0530

    Update ping.py

--- plugins/Devs/ping.py
+++ plugins/Devs/ping.py
@@ -2,7 +2,7 @@ import time
 from info import ADMINS
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("ping") & filters.users(ADMINS))
+@Client.on_message(filters.command("ping") & filters.user(ADMINS))
 async def ping(_, message):
     start_time = time.time()
     end_time = time.time()
