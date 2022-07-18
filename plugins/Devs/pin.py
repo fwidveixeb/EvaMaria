afbf981f513d3d31508163f50a99becf13d00764
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-18 10:53:44 +0530

    Create pin.py

---
+++ plugins/Devs/pin.py
@@ -0,0 +1,16 @@
+from info imort ADMINS
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.command("pin") & filters.user(ADMINS))
+async def pin(_, message: Message):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.pin()
+
+
+@Client.on_message(filters.command("unpin") & filters.user(ADMINS))
+async def unpin(_, message: Message):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.unpin()
