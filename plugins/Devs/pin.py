ddc979d6e5e3e132c6b9df4a382e9d4d9c7dac1f
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-18 11:01:36 +0530

    Update pin.py

--- plugins/Devs/pin.py
+++ plugins/Devs/pin.py
@@ -4,9 +4,22 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.command("pin") & filters.user(ADMINS))
 async def pin(_, message: Message):
-    if not message.reply_to_message:
-        return
-    await message.reply_to_message.pin()
+    
+    replied = message.reply_to_message
+    chat = message.chat.id
+    
+    await message.delete()
+    await replied.pin(disable_notification=true)
+    
+    except Exception as e:
+        await message.reply(f"#Error/n{e}")
+    
+    if not replied:
+        await message.delete
+        k = await message.reply(chat, "Reply to a message to Pin it.")
+        await asyncio.sleep(5)
+        await k.delete()
+        
 
 
 @Client.on_message(filters.command("unpin") & filters.user(ADMINS))
