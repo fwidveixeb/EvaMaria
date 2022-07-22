1138e9a0f7fa2fc89c4489d08c97816051126e7e
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 00:25:06 +0530

    Update pin.py

--- plugins/Devs/pin.py
+++ plugins/Devs/pin.py
@@ -21,21 +21,3 @@ async def pin(bot, message):
         await asyncio.sleep(5)
         await k.delete()
         
-@Client.on_message(filters.command("unpin") & filters.user(ADMINS))
-async def pin(bot, message):
-    
-    replied = message.reply_to_message
-    
-    if replied:
-        try:
-            await message.delete()
-            await replied.unpin()
-    
-        except Exception as e:
-            await message.reply(f"#Error {e}")
-    
-    if not replied:
-        await message.delete()
-        k = await message.reply("Reply to a message to Unpin it.")
-        await asyncio.sleep(5)
-        await k.delete()
