0c57cb4c89b8dec1088d050082cbffbd1717225a
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 00:47:12 +0530

    Update unpin.py

--- plugins/Devs/unpin.py
+++ plugins/Devs/unpin.py
@@ -1,7 +1,7 @@
 from info import ADMINS
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("unpin") & filters.user(ADMINS))
+Client.on_message(filters.command("unpin") & filters.user(ADMINS))
 async def pin(bot, message):
     
     replied = message.reply_to_message
@@ -12,10 +12,18 @@ async def pin(bot, message):
             await replied.unpin()
     
         except Exception as e:
-            await message.reply(f"#Error {e}")
+            k = await message.reply(f"#Error {e}")
+            await asyncio.sleep(5)
+            await k.delete()
     
     if not replied:
-        await message.delete()
-        k = await message.reply("Reply to a message to Unpin it.")
-        await asyncio.sleep(5)
-        await k.delete()
+        try:
+            await message.delete()
+            k = await message.reply("Reply to a message to Unpin it.")
+            await asyncio.sleep(5)
+            await k.delete()
+            
+        except Exception as e:
+            k = await message.reply(f"#Error {e}")
+            await asyncio.sleep(5)
+            await k.delete()
