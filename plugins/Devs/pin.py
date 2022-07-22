aba4c8305f1536ed9f40d4a28c0be5d1285e2fd2
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 00:15:13 +0530

    Update pin.py

--- plugins/Devs/pin.py
+++ plugins/Devs/pin.py
@@ -3,18 +3,15 @@ from info import ADMINS
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-@Client.on_message(filters.command("pin") & filters.user(ADMINS))
-async def pin(_, message: Message):
+@Client.on_message(filters.command("pin") & filters.regex(".pin") & filters.user(ADMINS))
+async def pin(bot, message):
     
     replied = message.reply_to_message
     
     if replied:
         try:
-            chat = message.chat.id
-            delm = message.message_id + 1
             await message.delete()
-            await replied.pin(disable_notification=True)
-            await Client.delete_messages(chat_id=chat, message_ids=delm)
+            await replied.pin()
     
         except Exception as e:
             await message.reply(f"#Error {e}")
@@ -25,10 +22,21 @@ async def pin(_, message: Message):
         await asyncio.sleep(5)
         await k.delete()
         
-
-
-@Client.on_message(filters.command("unpin") & filters.user(ADMINS))
-async def unpin(_, message: Message):
-    if not message.reply_to_message:
-        return
-    await message.reply_to_message.unpin()
+@Client.on_message(filters.command("unpin") & filters.regex(".unpin") & filters.user(ADMINS))
+async def pin(bot, message):
+    
+    replied = message.reply_to_message
+    
+    if replied:
+        try:
+            await message.delete()
+            await replied.unpin()
+    
+        except Exception as e:
+            await message.reply(f"#Error {e}")
+    
+    if not replied:
+        await message.delete()
+        k = await message.reply("Reply to a message to Unpin it.")
+        await asyncio.sleep(5)
+        await k.delete()
