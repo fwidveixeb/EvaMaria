418779576dd0b2a8f4855c7052cc55976e00f2c9
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 00:30:54 +0530

    Create delete.py

---
+++ plugins/Devs/delete.py
@@ -0,0 +1,14 @@
+from info import ADMINS
+from pyrogram import Client, filters
+
+@Client.on_message(filrers.command("d") & filters.user(ADMINS))
+async def del(bot, message):
+  
+  replied = message.reply_to_message
+  
+  if replied:
+    await replied.delete()
+    await message.delete()
+    
+  if not replied:
+    await message.delete()
