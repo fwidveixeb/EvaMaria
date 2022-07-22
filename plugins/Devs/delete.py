2bc41f4d496472fdc5ad17776a745ae60b5639fc
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 00:40:07 +0530

    Update delete.py

--- plugins/Devs/delete.py
+++ plugins/Devs/delete.py
@@ -1,14 +1,24 @@
 from info import ADMINS
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("d") & filters.user(ADMINS))
-async def del(bot, message):
-  
-  replied = message.reply_to_message
-  
-  if replied:
-    await replied.delete()
-    await message.delete()
+@Client.on_message(filters.command("pin") & filters.user(ADMINS))
+async def pin(bot, message):
     
-  if not replied:
-    await message.delete()
+    replied = message.reply_to_message
+    
+    if replied:
+        try:
+            await message.delete()
+            await replied.delete()
+        except Exception as e:
+            await k = message.reply(f"#Error {e}")
+            await asyncio.sleep(5)
+            await k.delete()
+            
+    if not replied:
+        try:
+            await message.delete()
+        except Exception as e:
+            await k = message.reply(f"#Error {e}")
+            await asyncio.sleep(5)
+            await k.delete()
