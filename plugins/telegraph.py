19778ef9a7272eea6c90d13363651ba3a868adea
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-24 01:25:31 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -102,12 +102,12 @@ async def telegraph(bot, message):
             title = "Hagadmansa"
             author_name = "Hagadmansa"
             author_url = "https://hagadmansa.com"
-            makeit = requests.get(f"https://api.telegra.ph/createPage?access_token={access_token}&title={title}&author_name={author_name}&author_url={author_url}&content=%5B%7B%22tag%22:%22p%22,%22children%22:%5B%22{replied.text}%22%5D%7D%5D&return_content=true").json()
+            makeit = requests.get(f"{API}?access_token={access_token}&title={title}&author_name={author_name}&author_url={author_url}&content=%5B%7B%22tag%22:%22p%22,%22children%22:%5B%22{replied.text}%22%5D%7D%5D&return_content=true").json()
             war = makeit["result"]["path"]
-            await b.edit(f"Here is your link:\n\nhttps://telegra.ph{war}", disable_web_page_preview=True)
-        except Exception as e:
+            await b.edit(f"Here is your link:\n\nhttps://telegra.ph/{war}", disable_web_page_preview=True)
+        except Exception:
             await b.delete()
-            await message.reply(f"#Error {e}\n\n Forward this to @HagadmansaBot.")
+            await message.reply(f"#Error {makeit}")
             
             
             
