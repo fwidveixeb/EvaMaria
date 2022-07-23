7b7bc34117c65aae259993549f400d716b68944e
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 17:44:22 +0530

    Update unpin.py

--- plugins/Devs/unpin.py
+++ plugins/Devs/unpin.py
@@ -1,7 +1,7 @@
 from info import ADMINS
 from pyrogram import Client, filters
 
-Client.on_message(filters.command("unpin") & filters.user(ADMINS))
+@Client.on_message(filters.command("unpin") & filters.user(ADMINS))
 async def unpin(bot, message):
     
     replied = message.reply_to_message
