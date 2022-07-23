a9c877e4fee5d7c404a760162c3e2748a67f4cb5
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-23 20:44:49 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -7,19 +7,18 @@ async def telegraph(bot, message):
     
     replied = message.reply_to_message
     
-    if replied:
-        if replied == message.photo:
-            p = await message.reply("Downloading...")
-            user_id = str(message.chat.id)
-            file_name_ = (f"./DOWNLOADS/{user_id}.jpg")
-            download = await bot.download_media(message=replied, file_name=file_name_)
-            await p.edit("Uploading...")
-            try:          
-                tgraph_link = upload_file(download)
-                await p.edit(f"Here is your link:\n\nhttps://telegra.ph{tgraph_link[0]}")     
-                os.remove(download) 
-            except Exception as e:
-                await message.reply(f"#Error {e}\n\n Forward this to @HagadmansaChat")
+    if replied.photo:
+        p = await message.reply("Downloading...")
+        user_id = str(message.chat.id)
+        file_name_ = (f"./DOWNLOADS/{user_id}.jpg")
+        download = await bot.download_media(message=replied, file_name=file_name_)
+        await p.edit("Uploading...")
+        try:         
+            tgraph_link = upload_file(download)
+            await p.edit(f"Here is your link:\n\nhttps://telegra.ph{tgraph_link[0]}")     
+            os.remove(download) 
+        except Exception as e:
+            await message.reply(f"#Error {e}\n\n Forward this to @HagadmansaChat")
                 
             
             
