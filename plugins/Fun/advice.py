84f949aa213a842621584c9415680c634c761e40
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-25 01:22:23 +0530

    Create advice.py

---
+++ plugins/Fun/advice.py
@@ -0,0 +1,12 @@
+import requests
+from pyrogram import Clienr, filters
+
+@Client.on_message(filters.command("advice"))
+async def advuce(bot, message):
+  k = await message.reply("processing..."
+  API = "https://api.safone.tech/advice")
+  m = requests.get(f"{API}").json()
+  n = m['advice']
+  o = f"{m} **-{message.from_user.mention}**"
+  await k.edit(f"{o}")
+  
