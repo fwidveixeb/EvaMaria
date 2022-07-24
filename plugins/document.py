b7c64b0cfac8cd15dbbc8a078366d8f1e39e2fce
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-24 20:26:25 +0530

    Update document.py

--- plugins/document.py
+++ plugins/document.py
@@ -4,6 +4,12 @@ from telegraph import Telegraph
 telegraph = Telegraph()
 telegraph.create_account(short_name="Hagadmansa")
 
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += ele 
+    return str1
+
 @Client.on_message(filters.command("abcd"))
 async def kdneidhd(bot, message):
   
@@ -19,8 +25,9 @@ async def kdneidhd(bot, message):
       pk = message.command[1:]
       if not pk:
         pk = "Hagadmansa"
+    monu = listToString(pk)
     try:
-      response = telegraph.create_page(title=f'{pk}', html_content=n, author_name="Hagadmansa", author_url="https://hagadmansa.com")
+      response = telegraph.create_page(title=f'{monu}', html_content=n, author_name="Hagadmansa", author_url="https://hagadmansa.com")
       await message.reply(f"Here is your link:\n\n{response['url']}", disable_web_page_preview=True)
       k.close()
     except Exception as e:
