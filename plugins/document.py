58dca81171e6a637888fa6f10945d667fc86eec8
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-24 20:59:05 +0530

    Update document.py

--- plugins/document.py
+++ plugins/document.py
@@ -14,21 +14,22 @@ async def kdneidhd(bot, message):
   replied = message.reply_to_message
   
   if replied.document:
+    input_str = message.command(1)
     path = (f"./DOWNLOADS/{message.chat.id}.txt")
     await bot.download_media(message=replied, file_name=path)
     k = open(path)
     p = k.read()
     n = p.replace("\n", "<br>")
-    if (message.command):
-      pk = message.command[1:]
-      if not pk:
+    if input_str:
+      pk = input_str
+    else:
         pk = "Hagadmansa"
     if pk == "Hagadmansa":
         monu = "Hagadmansa"
     else:
         monu = listToString(pk)
     try:
-      response = telegraph.create_page(title=[f'{pk}'], content=[f"{p}"], author_name="Hagadmansa", author_url="https://hagadmansa.com")
+      response = telegraph.create_page(title=pk, content=[f"{p}"], author_name="Hagadmansa", author_url="https://hagadmansa.com")
       await message.reply(f"Here is your link:\n\n{response['url']}", disable_web_page_preview=True)
       k.close()
     except Exception as e:
