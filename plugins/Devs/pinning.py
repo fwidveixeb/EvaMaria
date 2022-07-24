2dc95038f2fbbe8fde4da14c46a5376a5242506d
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-24 23:30:52 +0530

    Update and rename pin.py to pinning.py

--- plugins/Devs/pinning.py
+++ plugins/Devs/pinning.py
@@ -23,3 +23,23 @@ async def pin(bot, message):
         s = await message.reply("Reply to a message to Pin it.")
         await asyncio.sleep(5)
         await s.delete()
+@Client.on_message(filters.command("unpin") & filters.user(ADMINS))
+async def unpin(bot, message):
+    
+    replied = message.reply_to_message
+    
+    if replied:
+        try:
+            await message.delete()
+            await replied.unpin()
+    
+        except Exception as e:
+            k = await message.reply(f"#Error {e}")
+            await asyncio.sleep(5)
+            await k.delete()
+    
+    if not replied:
+        await message.delete()
+        s = await message.reply("Reply to a message to Unpin it.")
+        await asyncio.sleep(5)
+        await s.delete()
