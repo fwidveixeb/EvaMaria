147c410e28fd8336255651d462ea4b0018d18957
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-24 16:24:56 +0530

    Update document.py

--- plugins/document.py
+++ plugins/document.py
@@ -6,8 +6,9 @@ async def kdneidhd(bot, message):
   replied = message.reply_to_message
   
   if replied.document:
-    s = await bot.download_media(message=replied, file_name=f"{message.chat.id}.txt")
-    k = open(f"{message.chat.id}.txt")
+    path = (f"./DOWNLOADS/{message.chat.id}.txt")
+    s = await bot.download_media(message=replied, file_name=path)
+    k = open(path)
     p = k.read()
     await message.reply(f"{p}")
     k.close()
