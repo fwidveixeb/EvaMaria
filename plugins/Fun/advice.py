770aaa8c0c78c99faae463eeb3991fcdec71bfaa
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-25 01:25:06 +0530

    Update advice.py

--- plugins/Fun/advice.py
+++ plugins/Fun/advice.py
@@ -3,7 +3,7 @@ from pyrogram import Clienr, filters
 
 @Client.on_message(filters.command("advice"))
 async def advuce(bot, message):
-  k = await message.reply("processing..."
+  k = await message.reply("processing...")
   API = "https://api.safone.tech/advice"
   m = requests.get(f"{API}").json()
   n = m['advice']
