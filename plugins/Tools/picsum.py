d9704d5defea1b167bad13c390474bf6068d92bb
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-26 03:57:40 +0530

    Update picsum.py

--- plugins/Tools/picsum.py
+++ plugins/Tools/picsum.py
@@ -47,9 +47,9 @@ async def picsum(bot, message):
                 await picsum.edit(f"**COMMAND:**\n`{message.text}`\n\n**ERROR:**\n`{e}`\n\n**TIPS:**\n__1. Size must be in numbers.\n2. Size must be less than or equal to 5000.\n3. Pass 'True' in thirt argument to get a Black & White Image.\n4. Forward this to @HagadmansaChat.__")
                 os.remove("picsum.jpg")
                 
-        elif third == "1" or third == "2" or third == "3" or third == "4" or third == "5" or third == "6" or third == "7" or third == "8" or third == "9" or third == "10":
+        elif third in blur:
             try:     
-                response = requests.get(f"{API}/{height}/{width}?blur={third}")
+                response = requests.get(f"{API}/{height}/{width}/?blur={third}")
                 open("picsum.jpg", "wb").write(response.content)
                 await message.reply_photo("picsum.jpg")
                 await picsum.delete()
