ed440e4643ff8a74fa0f05221d9978f8fd9d8d66
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-26 00:33:00 +0530

    Update picsum.py

--- plugins/Tools/picsum.py
+++ plugins/Tools/picsum.py
@@ -5,26 +5,26 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command("picsum"))
 async def picsum(bot, message):
     
-  if len(message.command) == 1:
-    await message.reply("No size details provided, Read Help Menu First.")
+    r = message.text.split(None)
     
-  elif len(message.command) == 2:
-    await message.reply("Who will provide width?")
-    return
-  
-  r = message.text.split(None)
-  elif len(r) > 2:
-    picsum = await message.reply("processing")
-    height = message.text.split(None, 2)[1]
-    width = message.text.split(None, 2)[2]
-    print(height)
-    print(width)
-    API = "https://picsum.photos"
-    response = requests.get(f"{API}/{width}/{height}")
-    open("picsum.jpg", "wb").write(response.content)
-    await message.reply_photo("picsum.jpg")
-    await picsum.delete()
-    os.remove("picsum.jpg")
+    if len(message.command) == 1:
+        await message.reply("No size details provided, Read Help Menu First.")
     
-  else:
-    await message.reply("Something went wrong")
+    elif len(message.command) == 2:
+        await message.reply("Who will provide width?")
+    
+    elif len(r) > 2:
+        picsum = await message.reply("processing")
+        height = message.text.split(None, 2)[1]
+        width = message.text.split(None, 2)[2]
+        print(height)
+        print(width)
+        API = "https://picsum.photos"
+        response = requests.get(f"{API}/{width}/{height}")
+        open("picsum.jpg", "wb").write(response.content)
+        await message.reply_photo("picsum.jpg")
+        await picsum.delete()
+        os.remove("picsum.jpg")
+    
+      else:
+        await message.reply("Something went wrong")
