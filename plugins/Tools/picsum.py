4d6ebdc38f7ebb71dd738d4498eebc7f1aa81302
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-26 03:07:18 +0530

    Update picsum.py

--- plugins/Tools/picsum.py
+++ plugins/Tools/picsum.py
@@ -18,6 +18,8 @@ async def picsum(bot, message):
         picsum = await message.reply("`Processing...`")
         height = int(message.text.split(None, 2)[1])
         width = message.text.split(None, 2)[2]
+        if r != int(r):
+            await message.reply("message must be bumber")
         try:
             if height > 5000:
                 await message.reply("Height_error")
