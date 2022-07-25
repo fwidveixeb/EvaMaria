cfe8b3b9d06484881fcfb87d4c5eb09c0e31634b
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-26 00:05:30 +0530

    Update picsum.py

--- plugins/Tools/picsum.py
+++ plugins/Tools/picsum.py
@@ -6,17 +6,17 @@ from pyrogram import Client, filters
 async def picsum(bot, message):
   
   if not message.command:
-    return await message.reply("No arguments provided, Read Help menu first")
+    await message.reply("No arguments provided, Read Help menu first")
     
   if (message.command) == 1:
-    return await message.reply("Required 2 arguments, you provided only 1.")
+    await message.reply("Required 2 arguments, you provided only 1.")
     
   if (message.command) == 2:
     picsum = await message.reply("processing")
     height = message.command[1]
     width = message.command[2]
     API = "https://picsum.photos"
-    response = requests.get(API/width/height)
+    response = requests.get(f"{API}/{width}/{height}")
     open("picsum.jpg", "wb").write(response.content)
     await message.reply_photo("picsum.jpg")
     await picsum.delete()
