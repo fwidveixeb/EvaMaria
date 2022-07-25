b68242dc92b4a9a3dbaae73f7a508b6e08c3c0d4
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-26 02:05:00 +0530

    Update picsum.py

--- plugins/Tools/picsum.py
+++ plugins/Tools/picsum.py
@@ -14,10 +14,10 @@ async def picsum(bot, message):
         await message.reply("Who will provide width?")
     
     elif len(r) == 3:
+        API = "https://picsum.photos"
         picsum = await message.reply("processing")
         height = message.text.split(None, 2)[1]
         width = message.text.split(None, 2)[2]
-        API = "https://picsum.photos"
         try:
             response = requests.get(f"{API}/{height}/{width}")
             open("picsum.jpg", "wb").write(response.content)
@@ -29,21 +29,38 @@ async def picsum(bot, message):
             os.remove("picsum.jpg")
     
     elif len(r) == 4:
+        API = "https://picsum.photos"
+        blur = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         picsum = await message.reply("processing")
         height = message.text.split(None, 3)[1]
         width = message.text.split(None, 3)[2]
-        gray = message.text.split(None, 3)[3]
-        if gray != "True":
-            return await picsum.edit(f"**COMMAND:**\n`{message.text}`\n\n**OUTPUT:**\n`Third argument must be equal to 'True'`\n\n**TIPS:**\n__1. Size must be in numbers.\n2. Size must be less than or equal to 5000.\n3. If you need a Black & White Image pass 'True' as third argument.\n4. Forward this to @HagadmansaChat.__") 
-        API = "https://picsum.photos"
-        try:
-            response = requests.get(f"{API}/{height}/{width}?grayscale")
-            open("picsum.jpg", "wb").write(response.content)
-            await message.reply_photo("picsum.jpg")
-            await picsum.delete()
-            os.remove("picsum.jpg")
-        except Exception as e:
-            await picsum.edit(f"**COMMAND:**\n`{message.text}`\n\n**OUTPUT:**\n`{e}`\n\n**TIPS:**\n__1. Size must be in numbers.\n2. Size must be less than or equal to 5000.\n3. If you need a Black & White Image pass 'True' as third argument.\n4. Forward this to @HagadmansaChat.__")
-            os.remove("picsum.jpg")
+        third = message.text.split(None, 3)[3]
+        
+        if third == "True":
+            try:     
+                response = requests.get(f"{API}/{height}/{width}?grayscale")
+                open("picsum.jpg", "wb").write(response.content)
+                await message.reply_photo("picsum.jpg")
+                await picsum.delete()
+                os.remove("picsum.jpg")
+            except Exception as e:
+                await picsum.edit(f"**COMMAND:**\n`{message.text}`\n\n**OUTPUT:**\n`{e}`\n\n**TIPS:**\n__1. Size must be in numbers.\n2. Size must be less than or equal to 5000.\n3. Pass 'True' in thirt argument to get a Black & White Image.\n4. Forward this to @HagadmansaChat.__")
+                os.remove("picsum.jpg")
+                
+        elif third in blur:
+            try:     
+                response = requests.get(f"{API}/{height}/{width}?blur={blur}")
+                open("picsum.jpg", "wb").write(response.content)
+                await message.reply_photo("picsum.jpg")
+                await picsum.delete()
+                os.remove("picsum.jpg")
+            except Exception as e:
+                await picsum.edit(f"**COMMAND:**\n`{message.text}`\n\n**OUTPUT:**\n`Third argument must be equal to 'True' if need a Black & White Image or must be from 1 to 10 to get a blur Image.`\n\n**TIPS:**\n__1. Size must be in numbers.\n2. Size must be less than or equal to 5000.\n3. Pass a integer value between 1 to 10 to get blur Inage.\n4. Forward this to @HagadmansaChat.__")
+                os.remove("picsum.jpg")
+                    
+        else:
+            await picsum.edit(f"**COMMAND:**\n`{message.text}`\n\n**OUTPUT:**\n`{e}`\n\n**TIPS:**\n__1. Size must be in numbers.\n2. Size must be less than or equal to 5000.\n3. Pass a integer value between 1 to 10 to get blur Image.\n4. Pass 'True' in thirt argument to get a Black & White Image.\n5. Forward this to @HagadmansaChat.__")
+        
+        
     else:
         await message.reply("Something went wrong")
