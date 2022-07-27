3a972b969fe37a423e6a7988a512ea2015d7745e
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-28 03:29:24 +0530

    Update dobt.py

--- plugins/Tools/dobt.py
+++ plugins/Tools/dobt.py
@@ -57,12 +57,7 @@ async def dobt(bot, message):
       ab = pytz.timezone("Asia/Kolkata")
       abhi = dt.now(ab) 
       full = f"{d}/{m}/{y}"
-      
-      #Checking If everything is right or not?
-      try:
-        cd = dt.strptime(full, "%d/%m/%Y")  
-      except BaseException:
-        return await dobt.edit("Something went wrong in your Date Of Birth, please contact [Support](https://t.me/HagadmansaChat). ")
+      cd = dt.strptime(full, "%d/%m/%Y")
       
       #Getting Of Year
       abcd = ab.localize(cd)
@@ -99,42 +94,42 @@ async def dobt(bot, message):
       elif dan < 0:
           okk = 365 + ish
           bday = f"{okk} Days Left 🥳"
-      elif dan > 0:
+      else:
           bday = f"{ish} Days Left 🥳"
         
       #Determining Zodiac
-      if m == "12":
+      if b == "12":
           zodiac = "Sagittarius" if (d < 22) else "Capricorn"
-      elif m == "01":
+      elif b == "01":
           zodiac = "Capricorn" if (d < 20) else "Aquarius"
-      elif m == "02":
+      elif b == "02":
           zodiac = "Aquarius" if (d < 19) else "Pisces"
-      elif m == "03":
+      elif b == "03":
           zodiac = "Pisces" if (d < 21) else "Aries"
-      elif m == "04":
+      elif b == "04":
           zodiac = "Aries" if (d < 20) else "Taurus"
-      elif m == "05":
+      elif b == "05":
           zodiac = "Taurus" if (d < 21) else "Gemini"
-      elif m == "06":
+      elif b == "06":
           zodiac = "Gemini" if (d < 21) else "Cancer"
-      elif m == "07":
+      elif b == "07":
           zodiac = "Cancer" if (d < 23) else "Leo"
-      elif m == "08":
+      elif b == "08":
           zodiac = "Leo" if (d < 23) else "Virgo"
-      elif m == "09":
+      elif b == "09":
           zodiac = "Virgo" if (d < 23) else "Libra"
-      elif m == "10":
+      elif b == "10":
           zodiac = "Libra" if (d < 23) else "Scorpion"
-      elif m == "11":
+      elif b == "11":
           zodiac = "Scorpio" if (d < 22) else "Sagittarius"
     
       #Getting Horoscope
-      horoscope = pyaztro.Aztro(sign='Virgo')
+      horoscope = pyaztro.Aztro(sign=f"{zodiac}")
       description = horoscope.description
       today_date = horoscope.current_date
         
       #Creating Final output that would be sent to user
-      output = f"**Name:** `{name}`\n**D.O.B:** `{full}`\n**Life Lived:** `{lived}`\n**Upcoming Birthday:** `{bday}`\n**Zodiac:** `{bday}`\n\n**Horoscope on `{today_date}:`**\n`{description}`"
+      output = f"**Name:** `{name}`\n**D.O.B:** `{full}`\n**Life Lived:** `{lived}`\n**Upcoming Birthday:** `{bday}`\n**Zodiac:** `{zodiac}`\n\n**Horoscope on `{today_date}:`**\n`{description}`"
         
       #Sendind Data to the User
       return await dobt.edit(output)
