c1e298cbb49d2bae1a3f0b0b13812c282dd5f31d
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 12:16:18 +0530

    Update Vars.py

--- Vars.py
+++ Vars.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Var(object):
+    HEROKU_APP_NAME = environ['HEROKU_APP_NAME']
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", None))
     URL = f"https://download.hagadmansa.com/"
     BANNED_ID = environ.get("BANNED_ID", "")
