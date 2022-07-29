1b02a56f63e964066aefe4fc24919524da5c3658
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 15:47:48 +0530

    Update advice.py

--- plugins/Fun/advice.py
+++ plugins/Fun/advice.py
@@ -4,14 +4,14 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command("advice"))
 async def advice(bot, message):
   
-  k = await message.reply("`Processing...`")
+  advice = await message.reply("`Processing...`")
   API = "https://api.safone.tech/advice"
   try:
     m = requests.get(f"{API}").json()
     n = m['advice']
     o = f"{n}   ~**{message.from_user.first_name} {message.from_user.last}**"
-    await k.edit(f"{o}")
+    await advice.edit(f"{o}")
   except Exception as e:
-    await k.edit(f"#Error {e}\n\n Forward this to @HagadmansaChat")
+    await advice.edit(f"#Error {e}\n\n Forward this to @HagadmansaChat")
   
   
