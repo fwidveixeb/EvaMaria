a1320b4d710fbaa1c19755b8ea581c35bf5e7ac5
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 01:42:56 +0530

    Create thumb.py

---
+++ plugins/Tools/thumb.py
@@ -0,0 +1,17 @@
+from pyroogram import Clienr, filters
+
+@Client.on_message(filters.command("thumb"))
+async def thumb(bot, message_):
+  
+  thumb = await message.reply("`Processing...`")
+  replied = message.reply_to_message
+
+  if not replied.document:
+    return await thumb.edit("Reply to a file to download it's Thumbnail.")
+  
+  if not replied.document.thumbs:
+    return await thumb.edit("Replied file has no Thumbnail to show.")
+
+  await replied.download_media(thumb=-1)
+  m = await reply_photo(m)
+  os.remove(m)
