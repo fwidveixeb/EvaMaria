5b36857d50fef13b6a53645e361ce1617e6ba218
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 12:13:32 +0530

    Update Vars.py

--- Vars.py
+++ Vars.py
@@ -7,5 +7,5 @@ load_dotenv()
 class Var(object):
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", None))
     URL = f"https://download.hagadmansa.com/"
-    BANNED_ID = os.environ.get("BANNED_ID", "")
+    BANNED_ID = environ.get("BANNED_ID", "")
     TARGET_CHANNEL = int(environ.get("TARGET_CHANNEL", None)
