82a5714b6a2273284f0c397b049743ba2281a3f4
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 23:55:26 +0530

    Update img.py

--- plugins/Tools/img.py
+++ plugins/Tools/img.py
@@ -2,6 +2,10 @@ from shutil import rmtree
 from pyrogram import Client, filters
 from plugins.Helper.google_image import googleimagesdownload
 
+def listToString(s):
+    str1 = " "
+    return (str1.join(s))
+
 @Client.on_message(filters.command("img"))
 async def img(bot, message):
  
@@ -11,12 +15,12 @@ async def img(bot, message):
       return await img.edit("No query provided to search, Read Help Menu to know how command works.")
        
     elif len(message.command) > 1:
-      query = message.command[1:]
-      np = query[0]
+      hemlo = message.command[1:]
+      query = listToString(hemlo)
       limit = 5
       if ";" in query:
         try:
-            query = query.split(";")[0]
+            query = hemlo.split(";")[0]
             limit = int(query.split(";")[1])
         except BaseException:
             pass
@@ -29,8 +33,9 @@ async def img(bot, message):
           "output_directory": "./resources/",
       }
       path = await ggl.download(args)
-      ok = path[0][np]
-      await message.reply_media_group(media=ok)
+      ok = path[0][query]
+      print(ok)
+      #await message.reply_media_group(media=ok)
       rmtree(f"./resources/{query}/")
       await img.delete()
       
