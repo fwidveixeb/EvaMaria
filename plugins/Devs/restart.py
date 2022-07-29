d1e348792f697d7228f803afd7a65927b1428002
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 14:13:05 +0530

    Update restart.py

--- plugins/Devs/restart.py
+++ plugins/Devs/restart.py
@@ -5,11 +5,11 @@ from info import ADMINS
 from pyrogram import Client, Filters
 
 #Getting Heroku API KEY and APP NAME from Vars
-HEROKU_API_KEY = Var.HEROKU_IPI_KEY
+HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 
 heroku = heroku3.from_key(HEROKU_API_KEY)
-app = heroku.app("hagadmansabot2")
+app = heroku.app(HEROKU_APP_NAME)
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def restart(bot, message):
