0d56e147dc20758502631f2e05e5326d376027d4
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 14:17:22 +0530

    Update restart.py

--- plugins/Devs/restart.py
+++ plugins/Devs/restart.py
@@ -4,15 +4,14 @@ from Vars import Var
 from info import ADMINS
 from pyrogram import Client, Filters
 
-#Getting Heroku API KEY and APP NAME from Vars
-HEROKU_API_KEY = Var.HEROKU_API_KEY
-HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-
-heroku = heroku3.from_key(HEROKU_API_KEY)
-app = heroku.app(HEROKU_APP_NAME)
-
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def restart(bot, message):
+  
+  HEROKU_API_KEY = Var.HEROKU_API_KEY
+  HEROKU_APP_NAME = Var.HEROKU_APP_NAME
+
+  heroku = heroku3.from_key(HEROKU_API_KEY)
+  app = heroku.app(HEROKU_APP_NAME)
   
   k = await message.reply("`Restarting Bot... This may take time depending on the server.`"
   await app.restart()
