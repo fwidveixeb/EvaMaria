5a461a04c8ef6121d67310b8cccacea2839a9100
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 22:57:50 +0530

    Update img.py

--- plugins/Tools/img.py
+++ plugins/Tools/img.py
@@ -19,20 +19,19 @@ async def img(bot, message):
             limit = int(query.split(";")[1])
         except BaseException:
             pass
-      try:
-        ggl = googleimagesdownload()
-        args = {
-            "keywords": query,
-            "limit": limit,
-            "format": "jpg",
-            "output_directory": "./resources/",
-        }
-        path = await ggl.download(args)
-        ok = path[0][query]
-        await message.reply_photo(photo=ok, caption=query)
-        rmtree(f"./resources/{query}/")
-        await img.delete()
-      except Exception as e:
-        return await img.edit(f"#Error {e}")
+      
+      ggl = googleimagesdownload()
+      args = {
+          "keywords": query,
+          "limit": limit,
+          "format": "jpg",
+          "output_directory": "./resources/",
+      }
+      path = await ggl.download(args)
+      ok = path[0][query]
+      await message.reply_photo(photo=ok, caption=query)
+      rmtree(f"./resources/{query}/")
+      await img.delete()
+      
     else:
       return await img.edit("Parameter limit exceeded, Read Help Menu to know how command works.")
