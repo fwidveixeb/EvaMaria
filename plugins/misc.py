4cd503e13e07264be4dfc6fd31d26b93e5ac7311
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 15:13:17 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-Client.on_message(filters.command('id'))
+@Client.on_message(filters.command('id'))
 async def showid(client, message):
     chat_type = message.chat.type
     if chat_type == "private":
@@ -54,78 +54,6 @@ async def showid(client, message):
             quote=True
         )
 
-@Client.on_message(filters.command(["info"]))
-async def who_is(client, message):
-    # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
-    status_message = await message.reply_text(
-        "Fetching user information from Telegram..."
-    )
-    await status_message.edit(
-        "Processing User information to make it human redable..."
-    )
-    from_user = None
-    from_user_id, _ = extract_user(message)
-    try:
-        from_user = await client.get_users(from_user_id)
-    except Exception as error:
-        await status_message.edit(str(error))
-        return
-    if from_user is None:
-        return await status_message.edit("No valid user ID / message specified.")
-    message_out_str = ""
-    message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
-    last_name = from_user.last_name or "None"
-    message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
-    message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
-    username = from_user.username or "<b>None</b>"
-    dc_id = from_user.dc_id or "[User don't have any Profile Picture.]"
-    message_out_str += f"<b>➲Data Centre:</b> {dc_id}\n"
-    message_out_str += f"<b>➲User Name:</b> @{username}\n"
-    message_out_str += f"<b>➲User Link:</b> <a href='tg://user?id={from_user.id}'>Click Here</a>\n"
-    if message.chat.type in (("supergroup", "channel")):
-        try:
-            chat_member_p = await message.chat.get_member(from_user.id)
-            joined_date = datetime.fromtimestamp(
-                chat_member_p.joined_date or time.time()
-            ).strftime("%Y.%m.%d %H:%M:%S")
-            message_out_str += (
-                "<b>➲Joined this Chat on:</b> <code>"
-                f"{joined_date}"
-                "</code>\n"
-            )
-        except UserNotParticipant:
-            pass
-    chat_photo = from_user.photo
-    if chat_photo:
-        local_user_photo = await client.download_media(
-            message=chat_photo.big_file_id
-        )
-        buttons = [[
-            InlineKeyboardButton('🔐 Close', callback_data='close')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=local_user_photo,
-            quote=True,
-            reply_markup=reply_markup,
-            caption=message_out_str,
-            parse_mode="html",
-            disable_notification=True
-        )
-        os.remove(local_user_photo)
-    else:
-        buttons = [[
-            InlineKeyboardButton('🔐 Close', callback_data='close')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=message_out_str,
-            reply_markup=reply_markup,
-            quote=True,
-            parse_mode="html",
-            disable_notification=True
-        )
-    await status_message.delete()
 
 @Client.on_message(filters.command(["imdb"]))
 async def imdb_search(client, message):
