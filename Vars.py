04b0ae94aea6c2b8c96a1939ad3e79e46ba4be7d
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 14:12:28 +0530

    Update Vars.py

--- Vars.py
+++ Vars.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Var(object):
+    HEROKU_API_KEY = environ['HEROKU_API_KEY']
     HEROKU_APP_NAME = environ['HEROKU_APP_NAME']
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", None))
     URL = f"https://download.hagadmansa.com/"
