e57839004cf2d09219019a414c5f733b5315b71b
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 12:18:09 +0530

    Update Vars.py

--- Vars.py
+++ Vars.py
@@ -8,5 +8,4 @@ class Var(object):
     HEROKU_APP_NAME = environ['HEROKU_APP_NAME']
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", None))
     URL = f"https://download.hagadmansa.com/"
-    BANNED_ID = environ.get("BANNED_ID", "")
     TARGET_CHANNEL = int(environ.get("TARGET_CHANNEL", None)
