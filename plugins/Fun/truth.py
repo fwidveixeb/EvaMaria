b1f99ca67571c38c15d200ad7d485d13ff17ed67
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-29 16:27:58 +0530

    Create truth.py

---
+++ plugins/Fun/truth.py
@@ -0,0 +1,16 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("truth"))
+async def truth(bot, message):
+  
+  truth = await message.reply("`Processing...`")
+  API = "https://api.safone.tech/truth?category=classic"
+  try:
+    m = requests.get(f"{API}").json()
+    n = m['truth']
+    await truth.edit(f"{n}")
+  except Exception as e:
+    await truth.edit(f"#Error {e}\n\n Forward this to @HagadmansaChat")
+  
+  
