f752a294e8d26ab6d948c9a1564a85e4f7e02354
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 02:40:44 +0530

    Update url_checker.py

--- plugins/Helper/url_checker.py
+++ plugins/Helper/url_checker.py
@@ -1,10 +1,7 @@
 import requests
+from requests.exceptions import MissingSchema
 
 def is_url_ok(url: str):
-    try:
-        import requests
-    except ImportError:
-        raise DependencyMissingError("This function needs 'requests' to be installed.")
     try:
         r = requests.head(url)
     except MissingSchema:
