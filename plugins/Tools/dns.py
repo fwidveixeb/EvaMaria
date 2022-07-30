dc185462aca06ff5fa4092b77cdffbd4ba7318df
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 22:58:06 +0530

    Create dns.py

---
+++ plugins/Tools/dns.py
@@ -0,0 +1,21 @@
+import requests
+from pyrogram import Client, filters
+from plugins.Helper.url_checker import is_url_ok
+
+@Client.on_message(filters.command("dns"))
+async def dns(bot, message):
+  
+  dns = await message.reply("`Processing...`")
+  
+  if len(message.command) == 1:
+    return await dns.edit("No Server provided to check DNS, Read Help Menu to know how command works")
+  
+  elif len(message.command) == 2:
+    url = message.command[1]
+    if is_url_ok(url):
+      response = requests.get(f"https://da.gd/dns{url}").text
+      await dns.edit(text=f"{response}", disable_web_page_preview=True)
+    else: 
+      await dns.edit("Wrong URL provided.")
+  else:
+    await dns.edit("Parameter limit exceeded, Read Help Menu to know how command works.")  
