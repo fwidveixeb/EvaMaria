5bccce134480a18b53afe41a2ce8d0a1f568798a
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 23:41:19 +0530

    Create host.py

---
+++ plugins/Tools/host.py
@@ -0,0 +1,24 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("host"))
+async def host(bot, message):
+  
+  host = await message.reply("`Processing...`")
+  
+  if len(message.command) == 1:
+    return await host.edit("No Server provided to check HOST, Read Help Menu to know how command works")
+  
+  elif len(message.command) == 2:
+    url = message.command[1]
+    xurl = url.split('/')
+    mlist = ["http:", "https:"]
+    if xurl[0] in mlist:
+      final = xurl[2]
+      response = requests.get(f"https://da.gd/host/{final}").text
+      await host.edit(f"{response}")
+      
+    else:
+      await host.edit(f"URL must starts with http:// or https:// schema.")
+  else:
+    await host.edit("Parameter limit exceeded, Read Help Menu to know how command works.")  
