6877d605a1c74fc9db943ec08f8a5c06beb4296d
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 11:52:27 +0530

    Create glitch_maker.py

---
+++ plugins/Helper/glitch_maker.py
@@ -0,0 +1,18 @@
+import asyncio
+
+async def bash(cmd, run_code=0):
+    """
+    run any command in subprocess and get output or error."""
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip() or None
+    out = stdout.decode().strip()
+    if not run_code and err:
+        split = cmd.split()[0]
+        if f"{split}: not found" in err:
+            return out, f"{split.upper()}_NOT_FOUND"
+    return out, err
