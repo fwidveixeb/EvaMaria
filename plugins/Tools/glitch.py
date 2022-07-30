9305ff519da25b1572e79d6d42f30ee2d090bae3
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 12:14:18 +0530

    Update glitch.py

--- plugins/Tools/glitch.py
+++ plugins/Tools/glitch.py
@@ -6,13 +6,13 @@ from plugins.Helper.bash import bash
 async def glitch(bot, message):
   
   glitch = await message.reply("`Processing...`")
+  await bash("pip install -e git+https://github.com/1Danish-00/glitch_me.git#egg=glitch_me")
   replied = message.reply_to_message
   
   if not replied:
     return await glitch.edit('Reply to a photo to convert it to Glitch')
   
   elif replied.photo:
-    await bash("pip install -e git+https://github.com/1Danish-00/glitch_me.git#egg=glitch_me")
     poma = await bot.download_media(replied)
     userid = message.chat.id
     cmd = f"glitch_me gif --line_count 200 -f 10 -d 50 '{poma}' glitch_{userid}_.gif"
