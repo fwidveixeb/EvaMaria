24aa7540116803b3075f86c6b30a18a83a6c814f
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-30 23:48:23 +0530

    Create whois.py

---
+++ plugins/Tools/whois.py
@@ -0,0 +1,23 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("whois"))
+async def whois(bot, message):
+  
+  whois = await message.reply("`Processing...`")
+  
+  if len(message.command) == 1:
+    return await whois.edit("No URL provided to check Whois Details, Read Help Menu to know how command works")
+  
+  elif len(message.command) == 2:
+    url = message.command[1]
+    xurl = url.split('/')
+    mlist = ["http:", "https:"]
+    if xurl[0] in mlist:
+      final = xurl[2]
+      response = requests.get(f"https://da.gd/w/{final}").text
+      await whois.edit(f"{response}")
+    else:
+      await whois.edit(f"URL must starts with http:// or https:// schema.")
+  else:
+    await whois.edit("Parameter limit exceeded, Read Help Menu to know how command works.")  
