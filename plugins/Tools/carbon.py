20d271bbe22bb7f6f222132467bdef45461563ef
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-31 13:48:55 +0530

    Update carbon.py

--- plugins/Tools/carbon.py
+++ plugins/Tools/carbon.py
@@ -9,19 +9,16 @@ async def carbon(bot, message):
   replied = message.reply_to_message
   
   if replied:
-    if replied.document:
-      if replied.document.file_size < 5242880:
+    if replied.document and replied.document.file_size < 5242880:
+      try:
         down = await bot.download_media(replied)
         with open(down) as a:
           code = a.read()
           os.remove(down)
-      else:
-        try:
-          code = replied.text
-        except:
-          return await carbon.edit("File type not supported, Read Help Menu to know how command works.")
+      except:
+        return await carbon.edit("Reply to a text file only, Read Help Menu to know how command works.")
     else:
-      return await carbon.edit("Reply only to a Text or file, Read Help Menu to know how command works.")
+      code = replied.text
     pp = await Carbon(code=code, file_name=f"carbon_{message.chat.id}", backgroundColor="White")
     await message.reply_photo(pp)
     await carbon.delete()
