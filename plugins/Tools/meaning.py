0766ccad98efc092ce0512e2186d7bd0024b2f1e
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-31 18:07:04 +0530

    Create meaning.py

---
+++ plugins/Tools/meaning.py
@@ -0,0 +1,31 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_essage(filters.command("meaning"))
+async def meaning(bot, message):
+  
+  meaning = await message.reply("`Processing...`")
+  
+  if len(message.comand) == 1:
+     return await meaning.edit("No word provided to find meaning, Read Help Menu to know how command works")
+    
+  elif len(message.comand) == 2:
+    word = message.command[1]
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    out = requests.get(url).json()
+    try:
+        return await meaning.edit(f'**{out["title"]}**')
+    except (KeyError, TypeError):
+        pass
+    defi = out[0]["meanings"][0]["definitions"][0]
+    ex = "None" if not defi.get("example") else defi["example"]
+    text = "{}{}{}".format(wrd, defi["definition"], ex)
+    if len(text) > 4096: 
+        with io.BytesIO(str.encode(text)) as file: 
+            file.name = f"{wrd}-meaning.txt"
+            await message.reply_document(document=file, caption=f"Meanings of {wrd}", thumb="resources/devoloper.png")
+            return await meaning.delete()
+    else:
+        await meaning.edit(text)
+  else:
+    await meaning.edit("Parameter limit exceeded, Read Help Menu to know how command works.")
