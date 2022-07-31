9a892b918139f3ad567588331e69e65102b76061
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-01 00:29:48 +0530

    Create rmbg.py

---
+++ plugins/Helper/rmbg.py
@@ -0,0 +1,23 @@
+import aiohttp
+from Vars import Var
+from aiohttp import ContentTypeError
+
+RMBG_API = Var.RMBG_API
+
+async def RemoveBG(input_file_name):
+    RMBG_API = udB.get_key("RMBG_API")
+    headers = {"X-API-Key": RMBG_API}
+    files = {"image_file": open(input_file_name, "rb").read()}
+    async with aiohttp.ClientSession() as ses:
+        async with ses.post(
+            "https://api.remove.bg/v1.0/removebg", headers=headers, data=files
+        ) as out:
+            contentType = out.headers.get("content-type")
+            if "image" not in contentType:
+                return False, (await out.json())
+
+            name = check_filename(f"rmbg_{message.chat.id}.png")
+            file = await aiofiles.open(name, "wb")
+            await file.write(await out.read())
+            await file.close()
+            return True, name
