999809cd37e3c83141095cc4b058415553d83399
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-31 23:46:39 +0530

    Update spacebin.py

--- plugins/Tools/spacebin.py
+++ plugins/Tools/spacebin.py
@@ -15,15 +15,14 @@ async def spacebin(bot, message):
     msg = replied.text
     done, key = await get_paste(msg)
     if not done:
-        return await message.reply(key)
+        return await sb.edit(key)
     link = "https://spaceb.in/" + key
     raw = f"https://spaceb.in/api/v1/documents/{key}/raw"
-    await message.reply(
+    return await message.reply(
       text="Given text is successfully pasted on Spaceb.in",
       disable_web_page_preview=True,
-      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Spacebin', link), InlineKeyboardButton('Raw Text', raw)]])
+      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='Spacebin', url=link), InlineKeyboardButton(text='Raw Text', url=raw)]])
     )
-    return await sb.delete()
   
   if replied.document:
      if replied.document.file_size > 5242880:
