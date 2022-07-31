1e9a9280d9a526d7ed79f83512895e1c14fb9578
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-31 14:53:51 +0530

    Update carbon.py

--- plugins/Tools/carbon.py
+++ plugins/Tools/carbon.py
@@ -23,23 +23,23 @@ async def carbon(bot, message):
    if replied.photo and replied.video:
     return await carbon.edit("Reply only to a Text or file, Read Help Menu to know how command works.")
    if replied.document:
-      if replied.document.file_size > 5242880:
-        return await carbon.edit("Replied file must be less then 5 Mb.")
-      try:
-        down = await bot.download_media(replied)
-        with open(down) as a:
-          code = a.read()
-          os.remove(down)
-      except:
-        return await carbon.edit("Reply only to a Text or file, Read Help Menu to know how command works.")
-    else:
-      code = replied.text
-    if len(message.command) == 1:
-      bg = "White"
-    elif message.command[1] in ["random", "Random"]:
-      bg = rom
-    else:
-      bg = message.command[1]
-    pp = await Carbon(code=code, file_name=f"carbon_{message.chat.id}", backgroundColor=bg)
-    await message.reply_photo(pp)
-    await carbon.delete()
+    if replied.document.file_size > 5242880:
+     return await carbon.edit("Replied file must be less then 5 Mb.")
+    try:
+     down = await bot.download_media(replied)
+     with open(down) as a:
+      code = a.read()
+      os.remove(down)
+    except:
+      return await carbon.edit("Reply only to a Text or file, Read Help Menu to know how command works.")
+   else:
+    code = replied.text
+  if len(message.command) == 1:
+    bg = "White"
+  elif message.command[1] in ["random", "Random"]:
+    bg = rom
+  else:
+    bg = message.command[1]
+  pp = await Carbon(code=code, file_name=f"carbon_{message.chat.id}", backgroundColor=bg)
+  await message.reply_photo(pp)
+  await carbon.delete()
