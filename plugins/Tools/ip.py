57f846243ad95e45ffc79d2d61bc5bda1cc49195
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-01 01:23:09 +0530

    Create ip.py

---
+++ plugins/Tools/ip.py
@@ -0,0 +1,43 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("ip"))
+async def ip(bot, message):
+  
+  ip = await message.reply("`Processing...`")
+  
+  if len(message.command) == 1:
+    return await dagd.edit("No IP Address provided, Read Help Menu to know how command works")
+  
+  elif len(message.command) == 2:
+    ipp = message.command[1]
+    mota = await requests.get(f"https://ipinfo.io{ipp}/geo").json()
+    try:
+        err = mota["error"]["title"]
+        msg = mota["error"]["message"]
+        return await ip.edit(f"Error = {err}/nMessage = {msg}")
+    except:
+        pass
+    ipa = mota["ip"]
+    city = mota["city"]
+    region = mota["region"]
+    country = mota["country"]
+    cord = mota["loc"]
+    try:
+        zipc = mota["postal"]
+    except KeyError:
+        zipc = "None"
+    tz = det["timezone"]
+    data = f"""**IP Details Fetched**
+
+**IP:** `{ipa}`
+**City:** `{city}`
+**Region:** `{region}`
+**Country:** `{country}`
+**Co-ordinates:** `{cord}`
+**Postal Code:** `{zipc}`
+**Time Zone:** `{tz}`
+"""
+    return await ip.edit(data)
+  else:
+    await ip.edit("Parameter limit exceeded, Read Help Menu to know how command works.")  
