bafe5f17eb546cf4ee84aa07819169da479e699d
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-01 00:38:15 +0530

    Update rmbg.py

--- plugins/Tools/rmbg.py
+++ plugins/Tools/rmbg.py
@@ -1,56 +1,24 @@
-"""from pyrogram import Client, filters
+from pyrogram import Client, filters
+from plugins.Helper.rmbg import RemoveBG
 
-@ultroid_cmd(
-    pattern="rmbg($| (.*))",
-)
-async def abs_rmbg(event):
-    RMBG_API = udB.get_key("RMBG_API")
-    if not RMBG_API:
-        return await event.eor(
-            "Get your API key from [here](https://www.remove.bg/) for this plugin to work.",
-        )
-    match = event.pattern_match.group(1).strip()
-    reply = await event.get_reply_message()
-    if match and os.path.exists(match):
-        dl = match
-    elif reply and reply.media:
-        if reply.document and reply.document.thumbs:
-            dl = await reply.download_media(thumb=-1)
-        else:
-            dl = await reply.download_media()
-    else:
-        return await eod(
-            event, f"Use `{HNDLR}rmbg` as reply to a pic to remove its background."
-        )
-    if not (dl and dl.endswith(("webp", "jpg", "png", "jpeg"))):
-        os.remove(dl)
-        return await event.eor(get_string("com_4"))
-    if dl.endswith("webp"):
-        file = dl + ".png"
-        Image.open(dl).save(file)
-        os.remove(dl)
-        dl = file
-    xx = await event.eor("`Sending to remove.bg`")
-    dn, out = await ReTrieveFile(dl)
-    os.remove(dl)
-    if not dn:
-        dr = out["errors"][0]
-        de = dr.get("detail", "")
-        return await xx.edit(
-            f"**ERROR ~** `{dr['title']}`,\n`{de}`",
-        )
-    zz = Image.open(out)
-    if zz.mode != "RGB":
-        zz.convert("RGB")
-    wbn = check_filename("ult-rmbg.webp")
-    zz.save(wbn, "webp")
-    await event.client.send_file(
-        event.chat_id,
-        out,
-        force_document=True,
-        reply_to=reply,
-    )
-    await event.client.send_file(event.chat_id, wbn, reply_to=reply)
-    os.remove(out)
-    os.remove(wbn)
-    await xx.delete()"""
+@Client.on_message(filters.command("carbon"))
+async def carbon(bot, message):
+  
+  rmbg = await message.reply("`Processing...`")
+  replied = message.reply_to_message
+
+  if not replied:
+      return await rmgb.edit("Reply to a photo to Remove it's Backgroud, Read Help Menu to know how command works.")
+
+  if replied.photo:
+      photo = await bot.download_medua(replied)
+      dn, out = await RemoveBG(photo)
+      os.remove(photo)
+      if not dn:
+          dr = out["errors"][0]
+          de = dr.get("detail", "")
+          return await rmbg.edit(f"**ERROR ~** `{dr['title']}`,\n`{de}`")
+      await message.reply_photo(photo=out, caption="Here is your Image without Backgroud")
+      await rmbg.delete()
+      return os.remove(out)
+  await rmbg.edit("Reply only to a photo to Remove it's Background, Read Help Menu to know how command works.")
