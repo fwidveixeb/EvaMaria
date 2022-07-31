192f2c29a5abbb405675dcd81f7b782bb8a6dd7f
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-31 18:44:44 +0530

    Update meaning.py

--- plugins/Tools/meaning.py
+++ plugins/Tools/meaning.py
@@ -19,7 +19,7 @@ async def meaning(bot, message):
         pass
     defi = out[0]["meanings"][0]["definitions"][0]
     ex = "None" if not defi.get("example") else defi["example"]
-    text = f"**• Word: **`{word}`\n**• Meaning: **`{defi["definition"]}`\n**• Example: **__{ex}__"
+    text = "**• Word: **`{}`\n**• Meaning: **`{}`\n**• Example: **__{}__".format(word, defi["definition"], ex)
     if len(text) > 4096: 
         with io.BytesIO(str.encode(text)) as file: 
             file.name = f"{wrd}-meaning.txt"
