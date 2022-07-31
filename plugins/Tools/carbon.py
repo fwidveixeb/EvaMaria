f523fb7d0c958c2d057dd9d1c6ad31d4c62ed3d7
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-07-31 14:39:18 +0530

    Update carbon.py

--- plugins/Tools/carbon.py
+++ plugins/Tools/carbon.py
@@ -19,29 +19,24 @@ async def carbon(bot, message):
   carbon = await message.reply("`Processing...`")
   replied = message.reply_to_message
   
-  if replied:
-    if replied.document:
-      if replied.document.file_size > 5242880:
-        return await carbon.edit("Replied file must be less then 5 Mb.")
-      try:
-        down = await bot.download_media(replied)
-        with open(down) as a:
-          code = a.read()
-          os.remove(down)
-      except:
-        return await carbon.edit("Reply to a text file only, Read Help Menu to know how command works.")
+  if replied.text and replied.document:
+    if replied.document.file_size > 5242880:
+      return await carbon.edit("Replied file must be less then 5 Mb.")
+    try:
+      down = await bot.download_media(replied)
+      with open(down) as a:
+        code = a.read()
+        os.remove(down)
+    except:
+      return await carbon.edit("Reply to a text file only, Read Help Menu to know how command works.")
     else:
       code = replied.text
     if len(message.command) == 1:
       bg = "White"
-    elif len(message.command) == 2 and message.command[1] in ["random", "Random"]:
+    elif message.command[1] in ["random", "Random"]:
       bg = rom
-    elif len(message.command) == 2 and len(message.command[1]) == 6:
-      bg = message.command[1]
-    elif len(message.command) > 2:
-     return await carbon.edit("Parameter limit exceeded, Read Help Menu to know how command works.")  
     else:
-      return await carbon.edit(f"**COMMAND:**\n`{message.text}`\n\n**ERROR:**\n`Hagadmansa Says: [132 FIRST_PARAMETER_INVALID] - Firet Parameter should be 'Random' if need random colors, otherwise it should be a combination of 6 alphabets and numbers if need custom colors (Caused by 'Argument.ValueError')`")
+      bg = message.command[1]
     pp = await Carbon(code=code, file_name=f"carbon_{message.chat.id}", backgroundColor=bg)
     await message.reply_photo(pp)
     await carbon.delete()
