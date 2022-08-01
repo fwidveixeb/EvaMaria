8512f88602fb9a223828259515d5a4f20a907491
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-01 11:25:15 +0530

    Update json.py

--- plugins/Tools/json.py
+++ plugins/Tools/json.py
@@ -1,38 +1,26 @@
+import io
 from pyrogram import Client, filters
-from plugins.Helper.json import json_parser
 
 @Client.on_message(filters.command("json"))
 async def json(bot, message):
   
     json = await message.reply("`Processing...`")
     replied = message.reply_to_message
+    lomu = None
     
     if replied:
-      msg = replied
+        lomu = replied.stringify()
     else:
-      msg = message
-    try:
-      match = message.command[1]
-    except:
-      pass
-    if match and hasattr(msg, match):
-        msg = getattr(msg, match)
-        if hasattr(msg, "to_json"):
-            try:
-                msg = json_parser(msg.to_json(ensure_ascii=False), indent=1)
-            except Exception as e:
-                return await json.edit(e)
-        msg = str(msg)
-    else:
-        msg = json_parser(msg.to_json(), indent=1)
-    if len(msg) > 4096:
-        with io.BytesIO(str.encode(msg)) as out_file:
-            out_file.name = f"json_{message,chat.id}.txt"
-            await message.reply_document(
-              document=f"json_{message.chat.id}.txt",
+        lomu = message.stringify()
+        
+    if len(lomu) > 4096:
+        with io.BytesIO(str.encode(lomu)) as out_file:
+            out_file.name = f"json_{message.chat.id}.text"
+            await reply_document(
+              document=f"json_{message.chat.id}.text",
               caption="Here is your JSON data.",
-              thumb="resources/devoloper.png"
-            )
+              thumb="resources/dovoloper.png"
+            ) 
             return await json.delete()
     else:
-        await json.edit(f"```{msg or None}```")
+        await json.edit(lomu)
