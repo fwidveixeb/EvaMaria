f9e397f1ceb86cb77bb54635f59c1f50ec4ea217
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-02 02:40:37 +0530

    Create nekobin.py

---
+++ plugins/Tools/nekobin.py
@@ -0,0 +1,31 @@
+import requests
+from pyrogram import Client, filters
+"""
+@Client.on_message(filters.command("nekobin"))
+async def nekobin(bot, message):
+  
+  nb = await message.reply("`Processing...`")
+  replied = message.reply_to_message
+  
+  if not replied:
+    return await nb.edit(text="Reply to a Text or file to upload it on Spaceb.in, Read Help Menu to know how command works.", disable_web_page_preview=True)
+  
+  if replied.text:
+    hemlo = requests.get(f"https://open-apis-rest.up.railway.app/api/nekobin?text={replied.text}").json()
+    url = hemlo["data"]["url"]
+    return await nb.edit(f"Given text is successfully pasted on [Nekobin]({url}).")
+  
+  if replied.document:
+     if replied.document.file_size > 5242880:
+        return await nb.edit("Replied file must be less then 5 Mb.")
+     try:
+        down = await bot.download_media(replied)
+        with open(down) as a:  
+           msg = a.read()
+           os.remove(down)
+     except:
+        return await nb.edit("Reply only to a Text file only, Read Help Menu to know how command works.")
+     hemlo = requests.get(f"https://open-apis-rest.up.railway.app/api/nekobin?text={replied.text}").json()
+     url = hemlo["data"]["url"]
+     return await nb.edit(f"Given text is successfully pasted on [Nekobin]({url}).")
+  await nb.edit("Reply only to a Text or file, Read Help Menu to know how command works.")"""
