b66da09499864221e104ae09161ed54455d0462e
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-01 22:43:31 +0530

    Create pexels.py

---
+++ plugins/Tools/pexels.py
@@ -0,0 +1,26 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto
+
+@Client.on_message(filters.command("pexels"))
+async def pexels(bot, message):
+  
+    pexels = await message.reply("`Processing...`")
+    
+    if len(message.command) == 1:
+      return await pexels.edit("No query provided for Pexels, Read Help Menu to know how command works")
+    
+    if len(message.command) > 1:
+        nila = []
+        pila = []
+        query = message.command[1:]
+        pani = requests.get(f"https://api.pexels.com/v1/search?query={query}").json()
+        if pani["total_results"] == 0:
+          return await pexels.edit(f"No Results Found for {query}.")
+        for x in range(10):
+            photo = pani["photos"][x]["src"]["large2x"]
+            caption = pani["photos"][x]["alt"]
+            nila.append(photo)
+            pila.append(caption)
+        media = [InputMediaPhoto(nila[0], caption=pila[0]), InputMediaPhoto(nila[1], caption=pila[1]), InputMediaPhoto(nila[2], caption=pila[2]), InputMediaPhoto(nila[3], caption=pila[3]), InputMediaPhoto(nila[4], caption=pila[4]), InputMediaPhoto(nila[5], caption=pila[5]), InputMediaPhoto(nila[6], caption=pila[6]), InputMediaPhoto(nila[7], caption=pila[7]), InputMediaPhoto(nila[8], caption=pila[8]), InputMediaPhoto(nila[9], caption=pila[9])]
+        return await message.reply_media_group(media=media)
