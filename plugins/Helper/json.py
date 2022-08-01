b880749e46f9a677391f272e530be8d6db3a99a6
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-01 10:58:28 +0530

    Create json.py

---
+++ plugins/Helper/json.py
@@ -0,0 +1,19 @@
+import json
+from json.decoder import JSONDecodeError
+
+def json_parser(data, indent=None, ascii=False):
+    parsed = {}
+    try:
+        if isinstance(data, str):
+            parsed = json.loads(str(data))
+            if indent:
+                parsed = json.dumps(
+                    json.loads(str(data)), indent=indent, ensure_ascii=ascii
+                )
+        elif isinstance(data, dict):
+            parsed = data
+            if indent:
+                parsed = json.dumps(data, indent=indent, ensure_ascii=ascii)
+    except JSONDecodeError:
+        parsed = eval(data)
+    return parsed
