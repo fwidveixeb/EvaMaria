b306fe785b367c9c6b9b2c0c8699b0043d2cef40
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-02 19:46:38 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,17 +1,15 @@
-import random
-import asyncio
-from Vars import Var
-from info import PICS, ADMINS
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 
-HELP_TEXT = """**Welcome to the Help Menu**
+class txt(object):
+        HELP_TXT = """**Welcome to the Help Menu**
 
 Here you will find a detailed overview of every command with examples.
 
 Click on 'Open Help Menu' to open the detailed Menu."""
 
-HELP_BUTTONS = InlineKeyboardMarkup(
+class btn(object):
+        HELP_BTN = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton('Visit Website', url='https://hagadmansa.com')
             ],[
@@ -25,16 +23,16 @@ HELP_BUTTONS = InlineKeyboardMarkup(
 async def cb_data(bot, message):
     if message.data == "help":
         await message.answer('www.hagadmansa.com')
-        await message.edit(
-          caotion=HELP_TEXT,
-          reply_markup=HELP_BUTTONS
+        await message.edit_message_text(
+          text=HELP_TXT,
+          reply_markup=HELP_BTN
         )
 
 @Client.on_message(filters.command("help")) 
 async def help(client, message):
   
     await message.reply_photo(
-        photo=random.choice(PICS),
-        caption=HELP_TEXT,
-        reply_markup=HELP_BUTTONS
+      photo="https://telegra.ph/file/ebba73e063dac600db5d0.jpg",
+      caption=HELP_TXT,
+      reply_markup=HELP_BTN
     )
