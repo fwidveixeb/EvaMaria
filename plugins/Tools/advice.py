c9e4605547f7e507a5ce9d6430da1c3596ea8990
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-02 22:24:20 +0530

    Create advice.py

---
+++ plugins/Tools/advice.py
@@ -0,0 +1,15 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("advice"))
+async def advice(bot, message):
+  
+  advice = await message.reply("`Processing...`")
+  API = "https://api.safone.tech/advice"
+  try:
+    m = requests.get(f"{API}").json()
+    n = m['advice']
+    o = f"{n}   ~**{message.from_user.first_name} {message.from_user.last_name}**"
+    await advice.edit(f"{o}")
+  except Exception as e:
+    await advice.edit(f"#Error {e}\n\n Forward this to @HagadmansaChat")
