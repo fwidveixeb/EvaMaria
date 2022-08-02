3281935724d276752e02f9506789dfbae0c828a9
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-02 22:25:06 +0530

    Create dare.py

---
+++ plugins/Tools/dare.py
@@ -0,0 +1,14 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("dare"))
+async def dare(bot, message):
+  
+  dare = await message.reply("`Processing...`")
+  API = "https://api.safone.tech/dare?category=classic"
+  try:
+    m = requests.get(f"{API}").json()
+    n = m['dare']
+    await dare.edit(f"{n}")
+  except Exception as e:
+    await dare.edit(f"#Error {e}\n\n Forward this to @HagadmansaChat")
