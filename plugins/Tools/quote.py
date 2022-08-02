64e3b192df255b4e25356d21b20a223d628b4dfe
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-02 22:27:03 +0530

    Create quote.py

---
+++ plugins/Tools/quote.py
@@ -0,0 +1,15 @@
+import requests
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("quote"))
+async def quote(bot, message):
+  
+  quote = await message.reply("`Processing...`")
+  API = "https://api.safone.tech/quote"
+  try:
+    m = requests.get(f"{API}").json()
+    n = m['quote']
+    o = f"{n}   ~**{message.from_user.first_name} {message.from_user.last_name}**"
+    await quote.edit(f"{o}")
+  except Exception as e:
+    await quote.edit(f"#Error {e}\n\n Forward this to @HagadmansaChat")
