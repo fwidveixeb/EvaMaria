851540090d4fdfcc2f2325306f40860d7927df62
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-02 20:08:14 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -7,6 +7,8 @@ Here you will find a detailed overview of every command with examples.
 
 Click on 'Open Help Menu' to open the detailed Menu."""
 
+LIST_1_TXT = "12 Commands are listed here."
+
 HELP_BTN = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton('Visit Website', url='https://hagadmansa.com')
@@ -14,8 +16,31 @@ HELP_BTN = InlineKeyboardMarkup(
             InlineKeyboardButton('Updates', url='https://t.me/hagadmansa'),
             InlineKeyboardButton('Support', url='https://t.me/hagadmansachat')
             ],[
-            InlineKeyboardButton('Open Help Menu', callback_data='help')
+            InlineKeyboardButton('Open Help Menu', callback_data='list_1')
         ]])
+
+LIST_1_BTN = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('1', callback_data='1'),
+            InlineKeyboardButton('2', callback_data='2'),
+            InlineKeyboardButton('3', callback_data='3')
+            ],[
+            InlineKeyboardButton('1', callback_data='1'),
+            InlineKeyboardButton('2', callback_data='2'),
+            InlineKeyboardButton('3', callback_data='3')
+            ],[
+            InlineKeyboardButton('1', callback_data='1'),
+            InlineKeyboardButton('2', callback_data='2'),
+            InlineKeyboardButton('3', callback_data='3')
+            ],[
+            InlineKeyboardButton('1', callback_data='1'),
+            InlineKeyboardButton('2', callback_data='2'),
+            InlineKeyboardButton('3', callback_data='3')
+            ],[
+            InlineKeyboardButton('⇦', callback_data='1'),
+            InlineKeyboardButton('Home', callback_data='help'),
+            InlineKeyboardButton('⇨', callback_data='3')
+       ]])
 
 @Client.on_callback_query()
 async def cb_data(bot, message):
@@ -25,7 +50,13 @@ async def cb_data(bot, message):
           text=HELP_TXT,
           reply_markup=HELP_BTN
         )
-
+    elif message.data == "list_1":
+        await message.answer('www.hagadmansa.com')
+        await message.edit_message_text(
+          text=LIST_1_TXT,
+          reply_markup=LIST_1_BTN
+        )
+      
 @Client.on_message(filters.command("help")) 
 async def help(client, message):
   
