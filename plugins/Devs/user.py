4c13bf7691e6669dd950978e35154c3dab5e6c07
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-03 21:55:11 +0530

    Update user.py

--- plugins/Devs/user.py
+++ plugins/Devs/user.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-@Client.on_messagw(filters.command("user"))
+@Client.on_message(filters.command("user"))
 async def user(bot, message):
   user = message,command[1]
   k = await bot.get_users(user)
