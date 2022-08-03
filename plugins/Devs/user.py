2e0588be386eb6ffb6ee0b8112cba0251ab054ac
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-03 22:17:53 +0530

    Update user.py

--- plugins/Devs/user.py
+++ plugins/Devs/user.py
@@ -5,8 +5,8 @@ async def user(bot, message):
   file = message.command[1]
   await message.reply_cached_media(file)
 
-  @Client.on_message(filters.command("file"))
-  async def file(bot, message):
+@Client.on_message(filters.command("file"))
+async def file(bot, message):
     replied = message.reply_to_message
     media = replied.document or replied.video or replied.photo
     k = replied.file_id
