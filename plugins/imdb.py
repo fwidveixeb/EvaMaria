7d4e988e5015e4ca28a80f13a7e721358e90fc06
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-08 14:57:53 +0530

    Update imdb.py

--- plugins/imdb.py
+++ plugins/imdb.py
@@ -2,8 +2,9 @@ import os
 from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from info import IMDB_TEMPLATE
-from utils import extract_user, get_file_id, get_poster, last_online
+from utils import extract_user, get_file_id, last_online
 import time
+from plugins.Helper.imdb import get_poster
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
