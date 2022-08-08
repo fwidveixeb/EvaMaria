f4bdeaa0caefdc053264f761f7c0b23e11c89ae0
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-08 21:05:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -230,7 +230,7 @@ async def start(client, message):
         file_id = data
         pre = ""
     files_ = await get_file_details(file_id)           
-    elif not files_:
+    if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
             msg = await client.send_cached_media(
