60c80b29694975fbba9b7fa8018f03681a538f3f
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-10 16:22:22 +0530

    Update logs.py

--- plugins/Devs/logs.py
+++ plugins/Devs/logs.py
@@ -8,7 +8,9 @@ async def logs(bot, message):
         os.rename('TelegramBot.log', 'Hagadmansa.log')
         await message.reply_document(document='Hagadmansa.log', caption='Bot Logs')
     except FileNotFoundError:
-        await message.reply_document(document='Hagadmansa.log', caption='Bot Logs')
+        try:
+            await message.reply_document(document='Hagadmansa.log', caption='Bot Logs')
+        except Exception as e:
+            await message.reply(str(e))
     except Exception as e:
         await message.reply(str(e))
-    
