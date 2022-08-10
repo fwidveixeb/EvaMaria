0fe4ebcdb10580cdf287bb64f33ae8a5247f0d43
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-10 16:14:35 +0530

    Update logs.py

--- plugins/Devs/logs.py
+++ plugins/Devs/logs.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def logs(bot, message):
     try:
-        os.rename('TelegramBot.log', 'Hagadmanda.log')
+        os.rename('TelegramBot.log', 'Hagadmansa.log')
         await message.reply_document(document='Hagadmansa.log', caption='Bot Logs')
     except Exception as e:
         await message.reply(str(e))
