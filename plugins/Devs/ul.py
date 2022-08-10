88063839d35ce6eabbd81334ab2390de910eee93
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-10 13:28:05 +0530

    Update ul.py

--- plugins/Devs/ul.py
+++ plugins/Devs/ul.py
@@ -1,6 +1,7 @@
 import time
 from info import ADMINS
 from pyrogram import Client, filters
+from plugins.Helper.progress import progress
 
 @Client.on_message(filters.command("ul") & filters.user(ADMINS))
 async def ul(bot, message):
@@ -16,7 +17,8 @@ async def ul(bot, message):
   try:
     hemlo = await message.reply_document(
     document=lama,
-    thumb="resources/devoloper.png"
+    thumb="resources/devoloper.png",
+    progress=progress
     )
     await ul.delete()
     await hemlo.edit(f"Successfully uploaded `{lama}`.")
