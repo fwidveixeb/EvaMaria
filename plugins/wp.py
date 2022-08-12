3f17f830d989b3571051d2d455475beb83984d07
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-08-13 01:37:59 +0530

    Update wp.py

--- plugins/wp.py
+++ plugins/wp.py
@@ -1,14 +1,13 @@
 from info import ADMINS
-from bot import Bot
 from pyrogram import Client, filters
 
-@Bot.on_message(filters.command('wp') & filters.user(ADMINS))
+@Client.on_message(filters.command('wp') & filters.user(ADMINS))
 async def wp(bt, msg):
   
   wp = await message.reply('Process Started.')
-  first = await Bot.ask(chat.id, "Send me first text")
+  first = await Client.ask(chat.id, "Send me first text")
   first_text = first.txt
-  second = await Bot.ask(chat.id, "Send me second text")
+  second = await Client.ask(chat.id, "Send me second text")
   second_text = second.txt
   final = first_text + second_text
   await wp.edit(final)
