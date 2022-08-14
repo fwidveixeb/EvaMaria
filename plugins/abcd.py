81ed7fede3d2d435f85b37574617b3f737e82875
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 12:45:18 +0530

    Create abcd.py

---
+++ plugins/abcd.py
@@ -0,0 +1,14 @@
+from bot import bot
+from info import ADMINS
+from pyromod import listen
+from pyrogram import Client, filters
+
+@bot.on_message(filters.command('hello') & filters.user(ADMINS))
+async def genStr(bt, message):
+  
+  one = await bot.ask(message.chat.id, "send me a file.")
+  await message.reply_cached_media(one.document.file_id)
+  
+  two = await bot.ask(message.chat.id, "Now send me the Text")
+  await message.reply_cached_media(two.text)
+  
