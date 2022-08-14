e4f9d3d1f6b399ff6f050a25bbd8f1bfdfcbd638
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 12:48:33 +0530

    Update abcd.py

--- plugins/abcd.py
+++ plugins/abcd.py
@@ -1,14 +1,14 @@
-from bot import bot
 from info import ADMINS
-from pyromod import listen
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command('hello') & filters.user(ADMINS))
 async def genStr(bt, message):
   
-  one = await bot.ask(message.chat.id, "send me a file.")
-  await message.reply_cached_media(one.document.file_id)
+  await message.reply('hello')
   
-  two = await bot.ask(message.chat.id, "Now send me the Text")
-  await message.reply_cached_media(two.text)
+ # one = await bot.ask(message.chat.id, "send me a file.")
+ # await message.reply_cached_media(one.document.file_id)
+  
+ # two = await bot.ask(message.chat.id, "Now send me the Text")
+ # await message.reply_cached_media(two.text)
   
