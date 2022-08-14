50aa6ab2b3996209c4620492ae36bfba84dc3c13
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 17:44:56 +0530

    Update lala.py

--- lala.py
+++ lala.py
@@ -1,14 +1,16 @@
-from bot import bot
+#from bot import bot
 from info import ADMINS
-from pyromod import listen
+#from pyromod import listen
 from pyrogram import Client, filters
 
-@bot.on_message(filters.command("kaka") & filters.user(ADMINS))
+@Client.on_message(filters.command("lala") & filters.user(ADMINS))
 async def genStr(bt, message):
   
-  one = await bot.ask(message.chat.id, "send me a file.")
-  await message.reply_cached_media(one.document.file_id)
+  await message.reply('Hello')
   
-  two = await bot.ask(message.chat.id, "Now send me the Text")
-  await message.reply_cached_media(two.text)
+  #one = await bot.ask(message.chat.id, "send me a file.")
+  #await message.reply_cached_media(one.document.file_id)
+  
+  #two = await bot.ask(message.chat.id, "Now send me the Text")
+  #await message.reply_cached_media(two.text)
   
