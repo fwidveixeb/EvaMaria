06f4d91ef40ba180269a7bab73af13dcdf6ce7f2
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 17:48:06 +0530

    Update nana.py

--- plugins/nana.py
+++ plugins/nana.py
@@ -1,16 +1,16 @@
-#from bot import bot
+from bot import bot
 from info import ADMINS
-#from pyromod import listen
+from pyromod import listen
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("lala") & filters.user(ADMINS))
+@bot.on_message(filters.command("lala") & filters.user(ADMINS))
 async def genStr(bt, message):
   
   await message.reply('Hello')
   
-  #one = await bot.ask(message.chat.id, "send me a file.")
-  #await message.reply_cached_media(one.document.file_id)
+  one = await bot.ask(message.chat.id, "send me a file.")
+  await message.reply_cached_media(one.document.file_id)
   
-  #two = await bot.ask(message.chat.id, "Now send me the Text")
-  #await message.reply_cached_media(two.text)
+  two = await bot.ask(message.chat.id, "Now send me the Text")
+  await message.reply_cached_media(two.text)
   
