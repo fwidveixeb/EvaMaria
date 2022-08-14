de4c4ca16827eda1a2bbc60db52170db633ed46f
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 17:46:26 +0530

    Create nana.py

---
+++ plugins/nana.py
@@ -0,0 +1,16 @@
+#from bot import bot
+from info import ADMINS
+#from pyromod import listen
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("lala") & filters.user(ADMINS))
+async def genStr(bt, message):
+  
+  await message.reply('Hello')
+  
+  #one = await bot.ask(message.chat.id, "send me a file.")
+  #await message.reply_cached_media(one.document.file_id)
+  
+  #two = await bot.ask(message.chat.id, "Now send me the Text")
+  #await message.reply_cached_media(two.text)
+  
