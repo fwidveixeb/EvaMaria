9da3ffcc3992f9f111867311c4b80547255b41fd
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 11:56:29 +0530

    Update wp.py

--- wp.py
+++ wp.py
@@ -3,14 +3,14 @@ from info import ADMINS
 from pyromod import listen
 from pyrogram import Client, filters
 
-@bot.on_message(filters.private & filters.document & filters.user(ADMINS))
+@bot.on_message(filters.command('hello') & filters.user(ADMINS))
 async def genStr(bt, message):
   
-  one = await bt.get_messages(message.chat.id, message.id)
+  one = await bot.ask(message.chat.id, "send me a file.")
   await message.reply_cached_media(one.document.file_id)
   
-  two = await bot.ask(message.chat.id, "Now send me second document.")
-  await message.reply_cached_media(two.document.file_id)
+  two = await bot.ask(message.chat.id, "Now send me the Text")
+  await message.reply_cached_media(two.text)
   
 if __name__ == "__main__":
     app=bot()
