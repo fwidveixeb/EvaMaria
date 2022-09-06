ad0a04eee5e40c1c74f7de41d51665e46fd340fd
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-07 01:03:12 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -6,7 +6,4 @@ async def cmds(bot, message):
   
   cmds = await message.reply("`Processing...`")
   await asyncio.sleep(0.5)
-  await cmds.edit(
-    text='[Here](https://telegra.ph/Commands-of-Hagadmansa-Bot-08-05) are my all commands.',
-    disable_web_page_preview=True
-  )
+  await cmds.edit('[Here](https://telegra.ph/Commands-of-Hagadmansa-Bot-08-05) are my all commands.')
