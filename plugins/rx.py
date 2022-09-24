00373df4443e5fee641b45aa362717b8f71d77d7
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-24 22:57:58 +0530

    Update rx.py

--- plugins/rx.py
+++ plugins/rx.py
@@ -21,7 +21,7 @@ async def rename(bot, message):
     
     if (" " in message.text) and (replied is not None):
      
-        file_name = message.text.split('|')[0][3::]
+        file_name = message.text.split('|')[0][4::]
         caption = message.text.split('|')[1]
         
         time_ = time.time()
